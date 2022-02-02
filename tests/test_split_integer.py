d210fa125a754c24c5c3d680eb195bb49a65247f
IvanRamyk <ivan.r@mate.academy>
2022-02-02 11:15:36 +0200

    Add task

---
+++ tests/test_split_integer.py
@@ -0,0 +1,21 @@
+from app.split_integer import split_integer
+
+
+def test_sum_or_the_parts_should_be_equal_to_value():
+    pass
+
+
+def test_should_split_a_number_into_equal_parts_when_value_is_divisible_by_number_of_parts():
+    pass
+
+
+def test_should_return_a_part_equals_to_a_value_when_slitting_into_one_part():
+    pass
+
+
+def test_parts_should_be_sorted_when_they_are_not_equal():
+    pass
+
+
+def test_should_add_zeros_when_value_is_less_than_number_of_parts():
+    pass
