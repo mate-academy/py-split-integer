d8006fe33f9015baa7fc9f324d5fe5e35cfdf8d2
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-04 14:51:03 +0200

    'Solution'

--- tests/test_split_integer.py
+++ tests/test_split_integer.py
@@ -10,9 +10,7 @@ def test_should_split_a_number_into_equal_parts_when_value_is_divisible_by_numbe
 
 
 def test_should_return_a_part_equals_to_a_value_when_slitting_into_one_part():
-    goals = split_integer(18, 4)
-
-    assert goals[0] == 4
+    assert split_integer(18, 1) == [18]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
