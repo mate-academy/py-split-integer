567e3a0c62e2c0bbe15e6c75cf386b2ab771a592
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-04 12:50:44 +0200

    'Solution'

--- tests/test_split_integer.py
+++ tests/test_split_integer.py
@@ -16,14 +16,8 @@ def test_should_return_a_part_equals_to_a_value_when_slitting_into_one_part():
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
-    goals = split_integer(37, 9)
-    goals_to_compare = goals[:]
-
-    assert goals_to_compare == sorted(goals)
+    assert split_integer(37, 9) == [4, 4, 4, 4, 4, 4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
-    goals = split_integer(5, 7)
-
-    for num in range(7 - 5):
-        assert goals[num] == 0
+    assert split_integer(5, 7) == [0, 0, 1, 1, 1, 1, 1]
