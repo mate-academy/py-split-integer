1164060df5ada269f9cc14160ed59bca5408479c
Serhii Leonenko <leonenko3149@gmail.com>
2022-05-31 15:54:51 +0300

    Try to star autotest

--- tests/test_split_integer.py
+++ tests/test_split_integer.py
@@ -18,4 +18,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal():
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
-    assert split_integer(5, 8) == [0, 0, 0, 1, 1, 1, 1, 1]
+    assert split_integer(5, 7) == [0, 0, 1, 1, 1, 1, 1]
