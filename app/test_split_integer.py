c2993209aa472315397b088f4954290d8759d7d5
Serhii Leonenko <leonenko3149@gmail.com>
2022-06-01 13:54:27 +0300

    fixed second test name according to flake8

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -5,7 +5,7 @@ def test_sum_or_the_parts_should_be_equal_to_value():
     assert sum(split_integer(4, 2)) == 4
 
 
-def test_should_split_a_number_into_equal_parts_when_value_is_divisible_by_number_of_parts():
+def test_should_return_equal_parts_when_value_is_divisible_by_number_of_parts():
     assert split_integer(6, 2) == [3, 3]
 
 
