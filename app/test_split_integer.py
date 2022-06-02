3bb99381a3c596b731a114dbd2d5a3d13b07900e
Ed Orlovskyy <fedorov12816@gmail.com>
2022-06-02 15:43:50 +0300

    FlakeFixSolution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -11,7 +11,9 @@ def test_sum_or_the_parts_should_be_equal_to_value():
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
     value = 10
     number_of_parts = 2
-    assert split_integer(value, number_of_parts).count(value / number_of_parts) == number_of_parts
+    assert split_integer(value, number_of_parts). \
+        count(value / number_of_parts) == \
+           number_of_parts
     pass
 
 
@@ -32,5 +34,6 @@ def test_parts_should_be_sorted_when_they_are_not_equal():
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
     value = 2
     number_of_parts = 10
-    assert split_integer(value, number_of_parts) == [0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
+    assert split_integer(value, number_of_parts) ==\
+           [0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
     pass
