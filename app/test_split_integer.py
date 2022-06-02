9d5e801fe00a943013642eba28b21f98dd00bcb3
Hectorovich <andrew.gutnik10@gmail.com>
2022-06-02 17:32:33 +0300

    Solution2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_or_the_parts_should_be_equal_to_value():
     assert sum(goal) == 32
 
 
-def test_should_split_a_number_into_equal_parts_when_value_is_divisible_by_number_of_parts():
+def test_should_split_into_equal_parts_when_value_is_divisible_by_parts()::
     goal = split_integer(9, 3)
     assert len(set(goal)) == 1
 
