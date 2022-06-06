004ee908eee6268bfcd6fa014aed99c3be7b6cfa
Олег Белый <belochard@gmail.com>
2022-06-06 11:54:38 +0300

    Solution 3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -8,6 +8,8 @@ def test_sum_or_the_parts_should_be_equal_to_value():
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
     assert split_integer(value=16, number_of_parts=4) == [4, 4, 4, 4]
 
+    assert split_integer(value=17, number_of_parts=4) == [4, 4, 4, 5]
+
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
     assert split_integer(value=99, number_of_parts=1) == [99]
