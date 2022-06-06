be111b2c6ee88347fdbcc85c7d9873a579a48ac8
Vasyl Smutok <23.smutok@gmail.com>
2022-06-06 13:45:05 +0300

    at test_parts_should_be_sorted_when_they_are_not_equal specified the expected value

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -14,7 +14,7 @@ def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
-    assert split_integer(32, 6) == sorted(split_integer(32, 6))
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
