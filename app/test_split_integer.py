6376f90b7880020d0265838068880ed304f6151e
Ed Orlovskyy <fedorov12816@gmail.com>
2022-06-08 09:45:38 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -11,8 +11,8 @@ def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
     value = 10
     number_of_parts = 2
     assert split_integer(value, number_of_parts).count(
-        (value / number_of_parts) == number_of_parts
-    )
+        value / number_of_parts
+    ) == number_of_parts
 
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
