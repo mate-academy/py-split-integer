11ff82de16d77ff5af33f11c9e42c7ac7c5b4e52
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-21 15:42:58 +0300

    deleted blank lines

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -19,5 +19,3 @@ def test_parts_should_be_sorted_when_they_are_not_equal():
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
     assert split_integer(3, 4) == [0, 1, 1, 1]
-
-
