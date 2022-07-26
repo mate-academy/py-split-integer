aca716566d808e197af365fea5c5c255fd99b66a
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-07-26 19:43:14 +0300

    Update test_split_integer.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,11 +6,11 @@ def test_sum_or_the_parts_should_be_equal_to_value():
 
 
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
-    assert len(set(split_integer(6, 2))) == 1
+    assert split_integer(15, 5) == [3, 3, 3, 3, 3]
 
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
-    assert split_integer(8, 1)[0] == 8
+    assert split_integer(9, 1) == [9]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
@@ -18,4 +18,5 @@ def test_parts_should_be_sorted_when_they_are_not_equal():
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
-    assert split_integer(3, 5) == [0, 0, 1, 1, 1]
+    assert split_integer(4, 6) == [0, 0, 1, 1, 1, 1]
+    
