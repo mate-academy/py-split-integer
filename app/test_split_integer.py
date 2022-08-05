b958d672192eaeb53653ce30743ae3ef90eb60bc
stoveprofi <stoveprofi@gmail.com>
2022-08-05 18:12:45 +0100

    Corrected

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -7,8 +7,7 @@ def test_sum_or_the_parts_should_be_equal_to_value():
 
 
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
-    actual = split_integer(16, 4)
-    assert actual == [4, 4, 4, 4]
+    assert split_integer(16, 4) == [4, 4, 4, 4]
 
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
