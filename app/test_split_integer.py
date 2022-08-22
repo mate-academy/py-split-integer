da30d364552fb26bc8e97abde0282ac3f47c90a0
Yan Purdenko <purdenko22@gmail.com>
2022-08-22 14:01:02 +0200

    My Solution3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -3,15 +3,14 @@ import pytest
 from app.split_integer import split_integer
 
 
-class TestSplitInteger:
-    @pytest.mark.parametrize(
-        "value, number_of_parts, expected_list",
-        [
-            (8, 1, [8]),
-            (6, 2, [3, 3]),
-            (17, 4, [4, 4, 4, 5]),
-            (32, 6, [5, 5, 5, 5, 6, 6])
-        ]
-    )
-    def test_split_integer(self, value, number_of_parts, expected_list):
-        assert split_integer(value, number_of_parts) == expected_list
+@pytest.mark.parametrize(
+    "value, number_of_parts, expected_list",
+    [
+        (8, 1, [8]),
+        (6, 2, [3, 3]),
+        (17, 4, [4, 4, 4, 5]),
+        (32, 6, [5, 5, 5, 5, 6, 6])
+    ]
+)
+def test_split_integer(value, number_of_parts, expected_list):
+    assert split_integer(value, number_of_parts) == expected_list
