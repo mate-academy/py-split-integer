fdd2b8a87fd33697b2a24721e83f73f6caa2d2ff
yaroslav-demchenko <dastex1411@gmail.com>
2022-08-22 16:22:57 +0300

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,36 +1,22 @@
 from app.split_integer import split_integer
+import pytest
 
 
-def test_sum_or_the_parts_should_be_equal_to_value():
-    value = 32
-    number_of_parts = 6
-    res = split_integer(value, number_of_parts)
-    assert sum(res) == value
+class TestSplitInteger:
+    @pytest.mark.parametrize(
+        "value,number_of_parts,expected_result",
+        [
+            (32, 6, [5, 5, 5, 5, 6, 6]),
+            (6, 2, [3, 3]),
+            (8, 1, [8]),
+            (17, 4, [4, 4, 4, 5]),
+            (2, 5, [0, 0, 0, 1, 1])
+        ]
+    )
+    def test_returns_correct_values(self,
+                                    value,
+                                    number_of_parts,
+                                    expected_result):
 
-
-def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
-    value = 6
-    number_of_parts = 2
-    res = split_integer(value, number_of_parts)
-    assert res == [3, 3]
-
-
-def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
-    value = 8
-    number_of_parts = 1
-    res = split_integer(value, number_of_parts)
-    assert res == [value]
-
-
-def test_parts_should_be_sorted_when_they_are_not_equal():
-    value = 17
-    number_of_parts = 4
-    res = split_integer(value, number_of_parts)
-    assert res == sorted(res)
-
-
-def test_should_add_zeros_when_value_is_less_than_number_of_parts():
-    value = 2
-    number_of_parts = 5
-    res = split_integer(value, number_of_parts)
-    assert res == [0, 0, 0, 1, 1]
+        res = split_integer(value, number_of_parts)
+        assert res == expected_result
