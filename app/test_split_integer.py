983c75c39071e6b2c3561a34d80259a0946bc30c
Yan Purdenko <purdenko22@gmail.com>
2022-08-22 13:39:42 +0200

    My Solution2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,17 @@
+import pytest
+
 from app.split_integer import split_integer
 
 
-def test_sum_or_the_parts_should_be_equal_to_value():
-    assert split_integer(2, 2) == [1, 1]
-
-
-def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
-    assert split_integer(12, 3) == [4, 4, 4]
-
-
-def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
-    assert split_integer(8, 1) == [8]
-
-
-def test_parts_should_be_sorted_when_they_are_not_equal():
-    assert split_integer(7, 2) == [3, 4]
-
-
-def test_should_add_zeros_when_value_is_less_than_number_of_parts():
-    assert split_integer(1, 3) == [0, 0, 1]
+class TestSplitInteger:
+    @pytest.mark.parametrize(
+        "value, number_of_parts, expected_list",
+        [
+            (8, 1, [8]),
+            (6, 2, [3, 3]),
+            (17, 4, [4, 4, 4, 5]),
+            (32, 6, [5, 5, 5, 5, 6, 6])
+        ]
+    )
+    def test_split_integer(self, value, number_of_parts, expected_list):
+        assert split_integer(value, number_of_parts) == expected_list
