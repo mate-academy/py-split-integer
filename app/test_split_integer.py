897e53ba6b31063caac3624542af0ca1b9515adb
Oleg Usenko <Oleg911Usenko@gmail.com>
2022-08-26 22:30:09 +0300

    Complete task

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,46 @@ from app.split_integer import split_integer
 
 
 def test_sum_or_the_parts_should_be_equal_to_value():
-    pass
+    result = split_integer(8, 1)
+    assert sum(result) == 8
+
+    result = split_integer(6, 2)
+    assert sum(result) == 6
+
+    result = split_integer(17, 4)
+    assert sum(result) == 17
+
+    result = split_integer(32, 6)
+    assert sum(result) == 32
 
 
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
-    pass
+    result = split_integer(6, 2)
+    assert len(result) == 2
+
+    result = split_integer(17, 4)
+    assert len(result) == 4
+
+    result = split_integer(32, 6)
+    assert len(result) == 6
 
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
-    pass
+    result = split_integer(8, 1)
+    assert result[0] == 8
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
-    pass
+    result = split_integer(17, 4)
+    assert result == sorted(result)
+
+    result = split_integer(32, 6)
+    assert result == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
-    pass
+    result = split_integer(5, 6)
+    assert result == [0, 1, 1, 1, 1, 1]
+
+    result = split_integer(0, 3)
+    assert 0 in result
