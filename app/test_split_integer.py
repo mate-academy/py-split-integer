be3cb3987574c50a11ca391207ef75629214bc74
KevinXD <kevinfdevid@gmail.com>
2022-08-30 12:20:28 +0300

    Unnecessary variables were deleted

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,15 +2,11 @@ from app.split_integer import split_integer
 
 
 def test_sum_or_the_parts_should_be_equal_to_value():
-    value = 6
-    number_of_parts = 2
-    assert sum(split_integer(value, number_of_parts)) == value
+    assert sum(split_integer(6, 2)) == 6
 
 
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
-    value = 6
-    number_of_parts = 2
-    assert len(split_integer(value, number_of_parts)) == number_of_parts
+    assert len(split_integer(6, 2)) == 2
 
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
