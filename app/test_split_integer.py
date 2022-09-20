aabbeeeb797304e5ea43163b7b562f4d87ee4d18
Vlad Magdenko <vladearth7@gmail.com>
2022-09-20 18:09:58 +0300

    'Solution1'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -14,7 +14,7 @@ def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
-    assert split_integer(36, 5) == sorted(split_integer(36, 5))
+    assert split_integer(36, 5) == [7, 7, 7, 7, 8]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
