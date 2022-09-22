f74d85c5e1df5e2b22f709502097a4433a697501
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-09-22 16:32:20 +0300

    added tests for split integers to test_split_integer.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_or_the_parts_should_be_equal_to_value():
-    pass
+    assert sum(split_integer(12, 4)) == 12
 
 
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
-    pass
+    assert split_integer(8, 2) == [4, 4]
 
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
-    pass
+    assert split_integer(10, 1) == [10]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
-    pass
+    assert split_integer(5, 3) == [1, 2, 2]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
-    pass
+    assert split_integer(2, 4) == [0, 0, 1, 1]
