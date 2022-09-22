24a7e9b1cd1538e06593507b1bbe9cffbb1303f7
Davyd Kryvko <davidkrivko@gmail.com>
2022-09-22 14:53:10 +0200

    'Solution_2'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -10,7 +10,7 @@ def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
 
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
-    assert 30 == split_integer(30, 1)
+    assert [30] == split_integer(30, 1)
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
