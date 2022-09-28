34d9c55275168b3a1b7e7cd501bd55473b6f811e
Artem <Artemvyhovanec@gmail.com>
2022-09-28 16:47:06 +0300

    fixed some test

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -44,11 +44,11 @@ def test_sum_or_the_parts_should_be_equal_to_value():
 
 
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
-    assert len(split_integer(8, 4)) == 4
+    assert split_integer(8, 4) == [2, 2, 2, 2]
 
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
-    assert len(split_integer(8, 8)) == 8
+    assert split_integer(8, 1) == [8]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
@@ -56,8 +56,4 @@ def test_should_add_zeros_when_value_is_less_than_number_of_parts():
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
-    value = 9
-    number_of_parts = 2
-    result = split_integer(value, number_of_parts)
-
-    assert result == sorted(result)
+    assert split_integer(9, 2) == sorted(split_integer(9, 2))
