333de25a0df8125422b03bd655f73dbd5f1c5d53
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-09-29 19:43:14 +0300

    Changed tests according to the task

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_or_the_parts_should_be_equal_to_value():
-    assert split_integer(8, 1) == [8]
+    assert sum(split_integer(15, 3)) == 15
 
 
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
@@ -10,7 +10,7 @@ def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
 
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
-    assert split_integer(17, 4) == [4, 4, 4, 5]
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
