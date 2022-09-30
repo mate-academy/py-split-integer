d1f186aa5e02185dd81709ff6fa948c4789fa150
Maks Danylchuk <maxitosm00@gmail.com>
2022-09-30 18:09:01 +0300

    rdy

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,77 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value():
-    # preparation
-    value: int = 0
-    number_of_parts: int = 0
-
-    # action
-    result = split_integer(value, number_of_parts)
-
-    # check
-    assert number_of_parts == len(result)  # expected_classes
+    assert sum(split_integer(10, 2)) == 10
 
 
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
-    # preparation
-    value: int = 0
-    number_of_parts: int = 0
-    expected_result = []
-
-    # action
-    result = split_integer(value, number_of_parts)
-
-    for i in range(number_of_parts):
-        if value % number_of_parts == 0:
-            expected_result.append(value / number_of_parts)
-
-    # check
-    assert expected_result == result  # expected_classes
+    assert split_integer(20, 4) == [5, 5, 5, 5]
 
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
-    # preparation
-    value: int = 0
-    number_of_parts: int = 0
-    expected_answer: int = 0
-
-    # action
-    result = split_integer(value, number_of_parts)
-    if len(result) == 1:
-        if result[0] == value:
-            expected_answer = value
-
-    # check
-    assert expected_answer == value  # expected_classes
+    assert split_integer(15, 1) == [15]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal():
-    # preparation
-    value: int = 0
-    number_of_parts: int = 0
-    expected_result = []
-
-    # action
-    result = split_integer(value, number_of_parts)
-
-    for i in range(number_of_parts):
-        if value % number_of_parts != 0:
-            expected_result.append(round(value / number_of_parts) + 1)
-            value -= 1
-    # check
-    assert expected_result == result  # expected_classes
+    assert split_integer(17, 3) == [5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
-    # preparation
-    value: int = 0
-    number_of_parts: int = 0
-    expected_result = []
-
-    # action
-    result = split_integer(value, number_of_parts)
-
-    if value < number_of_parts:
-        for i in range(number_of_parts):
-            expected_result.append(0)
-    assert expected_result == result  # expected_classes
+    assert split_integer(3, 7) == [0, 0, 0, 0, 1, 1, 1]
