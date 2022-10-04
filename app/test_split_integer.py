66dd48bcf90678df50446b6bc3358d92e5772d58
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-10-04 17:48:16 +0300

    split_integer (2, 5)

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -18,4 +18,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal():
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts():
-    assert (2, 5) == [0, 0, 0, 1, 1]
+    assert split_integer(2, 5) == [0, 0, 0, 1, 1]
