a54f443a2262f836ac3310bfd48d1eb590e5a304
Yaroslav Havryliv <yaroslav.h@mate.academy>
2022-10-05 16:36:00 +0300

    annotations added

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,21 @@
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value():
+def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     pass
 
 
-def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
+def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     pass
 
 
-def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
+def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     pass
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal():
+def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     pass
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts():
+def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     pass
