6ad53a56285da6672765acf3b4bd15aa362a353b
Nikyxa <113314502+Nikyxa@users.noreply.github.com>
2022-10-05 21:40:30 +0300

    Update test_split_integer.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -3,14 +3,9 @@ from app.split_integer import split_integer
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     parts = split_integer(17, 4)
-=======
-    pass
 
     assert sum(parts) == 17
-
-=======
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    
 
 def test_should_split_into_equal_parts_when_value_is_divisible_by_parts() \
         -> None:
@@ -22,15 +17,6 @@ def test_should_split_into_equal_parts_when_value_is_divisible_by_parts() \
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part() \
         -> None:
     parts = split_integer(17, 1)
-=======
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
-
-
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
-
-    assert parts == [17]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -43,6 +29,3 @@ def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     parts = split_integer(2, 3)
 
     assert parts == [0, 1, 1]
-=======
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
