9180c859dc743cfb10a8f9fc4405d20ea62acddd
Nikyxa <kolisova.veronika81@gmail.com>
2022-10-05 14:49:25 +0300

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -7,13 +7,15 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(parts) == 17
 
 
-def test_should_split_into_equal_parts_when_value_is_divisible_by_parts() -> None:
+def test_should_split_into_equal_parts_when_value_is_divisible_by_parts() \
+        -> None:
     parts = split_integer(16, 4)
 
     assert parts == [4, 4, 4, 4]
 
 
-def test_should_return_part_equals_to_a_value_when_slitting_into_one_part() -> None:
+def test_should_return_part_equals_to_a_value_when_slitting_into_one_part() \
+        -> None:
     parts = split_integer(17, 1)
 
     assert parts == [17]
