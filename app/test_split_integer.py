b4350e9834354fefa1e138cdf00f4e2dd9678964
Yevhenii Davydenko <akazhulich@gmail.com>
2022-10-05 16:10:43 +0300

    add annotation, сut func name

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,21 @@
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value():
+def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
-def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
+def test_split_equal_parts_when_value_divisible_parts() -> None:
     assert split_integer(6, 2) == [3, 3]
 
 
-def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
+def test_return_part_equals_value_when_slitting_one_part() -> None:
     assert split_integer(8, 1) == [8]
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal():
+def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts():
+def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(1, 3) == [0, 0, 1]
