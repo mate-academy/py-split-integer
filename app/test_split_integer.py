d3b4e07801023456c5375bc4fb40cb1e51380e96
Sergey <servizah@gmail.com>
2022-10-05 18:13:18 +0300

    removed unnesessary tab

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,22 +1,22 @@
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value():
+def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(17, 4)) == 17
 
 
-def test_should_split_into_equal_parts_when_value_is_divisible_by_parts():
+def test_should_split_into_equal_parts_when_value_is_divisible_by_parts() -> None:
     assert split_integer(16, 4) == [4, 4, 4, 4]
 
 
-def test_should_return_part_equals_to_a_value_when_slitting_into_one_part():
+def test_should_return_part_equals_to_a_value_when_slitting_into_one_part() -> None:
     assert split_integer(13, 1) == [13]
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal():
+def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     parts_list = split_integer(32, 5)
     assert parts_list == sorted(parts_list)
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts():
+def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(3, 5) == [0, 0, 1, 1, 1]
