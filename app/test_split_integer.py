fcb644347f3d3b749e152b2af468235344a1a27c
Igor Kosianenko <funkcionalkos@gmail.com>
2022-10-06 14:46:55 +0300

    Update test_split_integer.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -13,7 +13,7 @@ def test_should_split_into_equal_parts_when_value_is_divisible_by_parts() -> Non
 
 def test_should_return_part_equals_to_a_value_when_slitting_into_one_part() -> None:
 
-    assert split_integer(8, 1) == 8
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
