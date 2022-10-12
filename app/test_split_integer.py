19e821113461012bbd7816966e9cc1ee75b1a4b2
LavrykN <nlavryk@kaaiot.io>
2022-10-12 09:56:56 +0300

    Refactoring

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -14,7 +14,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(12, 2) == [6, 6]
+    assert split_integer(16, 5) == [3, 3, 3, 3, 4]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
