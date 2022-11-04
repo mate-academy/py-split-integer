f5e44886848e19f1258c7d2ac2ed5bdfe2e21f06
Taras Martyniuk <bythewaters800@gmail.com>
2022-11-04 13:59:55 +0100

    Sol2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,29 +2,25 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    parts = split_integer(32, 6)
 
-    assert sum(parts) == 32
+    assert sum(split_integer(32, 6)) == 32
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    parts = split_integer(6, 2)
 
-    assert parts == [3, 3]
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    parts = split_integer(6, 1)
-    assert parts[0] == 6
+
+    assert split_integer(6, 1)[0] == 6
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    parts = split_integer(32, 6)
 
-    assert parts == sorted(parts)
+    assert split_integer(32, 6) == sorted(split_integer(32, 6))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    parts = split_integer(5, 6)
 
-    assert parts[0] == 0
+    assert split_integer(5, 6)[0] == 0
