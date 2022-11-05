b61ce728f6a52d3d0a9678b5083aaea6f7db17c9
Taras Martyniuk <bythewaters800@gmail.com>
2022-11-05 14:27:33 +0100

    Sol3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -13,7 +13,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
-    assert split_integer(6, 1)[0] == 6
+    assert split_integer(6, 1) == [6]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -23,4 +23,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
 
-    assert split_integer(5, 6)[0] == 0
+    assert 0 in split_integer(5, 6)
