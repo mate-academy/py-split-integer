afa6ca46a99f3c5799558d2c99fc4d1583990a4b
Andrew.H <john.riderr9@gmail.com>
2022-11-05 13:15:35 +0200

    flake8 fix2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -17,9 +17,8 @@ def test_sum_of_the_parts_should_be_equal_to_value(
         number_of_parts: int
 ) -> None:
     parts = split_integer(value, number_of_parts)
-    assert (
-            sum(parts) == value
-    ), f"Sum of the {parts} should be equal to {value}"
+    assert (sum(parts) == value),\
+        f"Sum of the {parts} should be equal to {value}"
 
 
 @pytest.mark.parametrize(
@@ -36,9 +35,8 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(
         number_of_parts: int
 ) -> None:
     parts = split_integer(value, number_of_parts)
-    assert (
-            parts.count(parts[0]) == len(parts)
-    ), "all of the parts should be equal when value divisible by parts"
+    assert (parts.count(parts[0]) == len(parts)),\
+        "all of the parts should be equal when value divisible by parts"
 
 
 @pytest.mark.parametrize(
@@ -53,9 +51,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part(
         value: int,
         number_of_parts: int
 ) -> None:
-    assert (
-            split_integer(value, number_of_parts)[0] == value
-    ), "When value split into one part, return value should be equal to value"
+    assert (split_integer(value, number_of_parts)[0] == value),\
+        "When value split into one part, return value should be equal to value"
 
 
 @pytest.mark.parametrize(
