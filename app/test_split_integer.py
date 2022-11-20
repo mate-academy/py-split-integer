01a33a0c4289d211c096e2acfeb0e002d00a97db
Admin <winofkill@gmail.com>
2022-11-20 12:42:31 +0200

    DONE2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -3,29 +3,29 @@ from app.split_integer import split_integer
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert (
-            sum(split_integer(21, 2)) == 21
+        sum(split_integer(21, 2)) == 21
     )
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert (
-            split_integer(25, 5) == [5, 5, 5, 5, 5]
+        split_integer(25, 5) == [5, 5, 5, 5, 5]
     )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert (
-            split_integer(25, 1) == [25]
+        split_integer(25, 1) == [25]
     )
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert (
-            split_integer(23, 3) == sorted(split_integer(23, 3))
+        split_integer(23, 3) == sorted(split_integer(23, 3))
     )
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert (
-            split_integer(3, 5) == [0, 0, 1, 1, 1]
+        split_integer(3, 5) == [0, 0, 1, 1, 1]
     )
