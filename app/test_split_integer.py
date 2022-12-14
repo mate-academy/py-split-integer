30f7238d8470c0e496145c36ddf1ba563e257ab6
Kostyantyn Chernyk <chernykko@gmail.com>
2022-12-14 13:27:04 +0000

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,31 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    goals = split_integer(6, 2)
-
-    assert sum(goals) == 6, "Sum of array should be equal to value"
+    assert sum(split_integer(6, 2)) == 6
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    goals = split_integer(6, 2)
-
-    assert all(part == goals[0] for part in goals), \
-        "All elements of array should be equal"
+    assert all(part == split_integer(6, 2)[0] for part in split_integer(6, 2))
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    goals = split_integer(8, 1)
-
-    assert len(goals) == 1, "Array should have only 1 element"
+    assert split_integer(8, 1)[0] == 8
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    goals = split_integer(17, 4)
-
-    assert sorted(goals) == goals, "Array should be sorted"
+    assert sorted(split_integer(17, 4)) == split_integer(17, 4)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    goals = split_integer(3, 4)
-
-    assert goals.count(0) == 1, "Array should add correct number of zeros"
+    assert split_integer(3, 4).count(0) == 1
