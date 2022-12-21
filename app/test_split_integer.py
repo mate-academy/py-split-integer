e5b440effe8fc084f3b79bfc979293958128d5bc
Andriy <an_mydel@gmail.com>
2022-12-21 05:26:31 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -12,29 +12,32 @@ from app.split_integer import split_integer
         (32, 6, 32)
     ]
 )
-def test_sum_of_the_parts_should_be_equal_to_value(value, parts, expected) -> None:
+def test_sum_of_the_parts_should_be_equal_to_value(
+        value: int,
+        parts: int,
+        expected: int) -> None:
     assert (
         sum(split_integer(value, parts)) == expected
     ), f"Sum of split_integer({value}) must equal to {value}"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert(
+    assert (
         len(set(split_integer(4, 2))) == 1
     )
-    assert(
+    assert (
         len(set(split_integer(1, 1))) == 1
     )
-    assert(
+    assert (
         len(set(split_integer(63, 7))) == 1
     )
-    assert(
+    assert (
         len(set(split_integer(187, 17))) == 1
     )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert(split_integer(3, 1)[0] == 3)
+    assert (split_integer(3, 1)[0] == 3)
     assert (split_integer(10, 1)[0] == 10)
     assert (split_integer(23, 1)[0] == 23)
     assert (split_integer(1, 1)[0] == 1)
@@ -49,9 +52,11 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
         (765, 54)
     ]
 )
-def test_parts_should_be_sorted_when_they_are_not_equal(value, parts) -> None:
+def test_parts_should_be_sorted_when_they_are_not_equal(
+        value: int,
+        parts: int) -> None:
     result = split_integer(value, parts)
-    assert(
+    assert (
         result == sorted(result)
     )
 
@@ -65,7 +70,9 @@ def test_parts_should_be_sorted_when_they_are_not_equal(value, parts) -> None:
         (43, 89)
     ]
 )
-def test_should_add_zeros_when_value_is_less_than_number_of_parts(value, parts) -> None:
-    assert(
+def test_should_add_zeros_when_value_is_less_than_number_of_parts(
+        value: int,
+        parts: int) -> None:
+    assert (
         sum(split_integer(value, parts)[:parts - value]) == 0
     )
