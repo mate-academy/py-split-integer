4f1f4eff745de4ee840e884eea3f7577979a2dfc
Derkach Oleksandr <derkach03553@gmail.com>
2022-12-22 14:12:07 +0200

    fixed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(15, 3)[0] == split_integer(15, 3)[-1]
+    assert split_integer(15, 3) == [5, 5, 5]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
