0323e83f964bcb18d4267a14acd57e5f7a800562
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-01-04 11:41:00 +0200

    resolve remarks

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,15 +2,14 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    goal = split_integer(32, 6)
 
-    assert sum(goal) == 32
+    assert sum(split_integer(32, 6)) == 32
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     goal = split_integer(30, 6)
 
-    assert all(True if num == 5 else False for num in goal)
+    assert all(num == 5 for num in goal)
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -20,10 +19,9 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     goal = split_integer(32, 6)
-    assert all(True if goal[i] == sorted(goal)[i]
-               else False for i in range(len(goal)))
+    assert goal == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    goal = split_integer(2, 12)
-    assert goal[0] == 0
+    goal = split_integer(2, 3)
+    assert goal == [0, 1, 1]
