e3c4af99b2587cd1a073165d8e610d95c99ee16b
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-01-05 08:54:58 +0200

    Split integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -7,21 +7,20 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    goal = split_integer(30, 6)
 
-    assert all(num == 5 for num in goal)
+    assert all(num == 5 for num in split_integer(30, 6))
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    goal = split_integer(32, 1)
-    assert goal[0] == 32
+
+    assert split_integer(32, 1) == [32]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    goal = split_integer(32, 6)
-    assert goal == [5, 5, 5, 5, 6, 6]
+
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    goal = split_integer(2, 3)
-    assert goal == [0, 1, 1]
+
+    assert split_integer(2, 3) == [0, 1, 1]
