c27bfa7a48499bf55e9318f2c1d638737157615c
MIlan <milan.manoylov@gmail.com>
2023-01-11 14:13:23 -0600

    split_integer_Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,30 +2,30 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    parts = split_integer(8, 1)
+    parts = split_integer(32, 6)
 
-    assert sum(parts) == 8
+    assert sum(parts) == 32
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    parts = split_integer(10, 2)
+    parts = split_integer(6, 2)
 
-    assert parts == [5, 5]
+    assert parts == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    parts = split_integer(10, 5)
+    parts = split_integer(8, 1)
 
-    assert parts == [2, 2, 2, 2, 2]
+    assert parts == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    parts = split_integer(32, 6)
+    parts = split_integer(17, 4)
 
-    assert parts == [5, 5, 5, 5, 6, 6]
+    assert sorted(parts) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    parts = split_integer(32, 6)
+    parts = split_integer(5, 6)
 
-    assert parts == sorted(parts)
+    assert parts == [0, 1, 1, 1, 1, 1]
