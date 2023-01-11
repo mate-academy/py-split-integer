f84abec07c266337828febb9713258ec0c752be2
IhorPokr <pokroveckijigor@gmail.com>
2023-01-11 11:25:19 +0100

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -19,4 +19,3 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(32, 6) == sorted(split_integer(32, 6))
-
