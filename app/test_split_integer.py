54c02a52d55c71cea47e7c6c6237bec69c477fba
Taras Savchyn <savik1992@gmail.com>
2023-02-01 14:01:07 +0200

    change test 2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -7,7 +7,6 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert split_integer(16, 4) == [4, 4, 4, 4]
-    assert split_integer(18, 4) == [4, 4, 5, 5]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -15,7 +14,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert sorted(split_integer(32, 6)) == split_integer(32, 6)
+    assert sorted(split_integer(32, 6)) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
