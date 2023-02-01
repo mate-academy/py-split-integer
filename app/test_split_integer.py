92e8981ae355b625125a11d5298c20c70173d96d
Paul Maslov <maslovp67@gmail.com>
2023-02-01 15:02:04 +0200

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,25 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result_list = split_integer(17, 4)
-    assert len(result_list) == 4
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result_list = split_integer(32, 4)
-    assert result_list == [8, 8, 8, 8]
+    assert split_integer(32, 4) == [8, 8, 8, 8]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    result_list = split_integer(17, 1)
-    assert result_list == [17]
+    assert split_integer(17, 1) == [17]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result_list = split_integer(32, 6)
-    assert result_list == [5, 5, 5, 5, 6, 6]
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result_list = split_integer(3, 6)
-    assert result_list == [0, 0, 0, 1, 1, 1]
+    assert split_integer(3, 6) == [0, 0, 0, 1, 1, 1]
