01de7e91a1f6e9c8e5262ba81842006b9cc9abf6
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-15 22:10:59 +0200

    2 commit

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    assert sum(split_integer(32, 6)) == sum([5, 5, 5, 5, 6, 6])
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
