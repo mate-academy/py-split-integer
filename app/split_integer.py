9d42401e7bb94d1196737085e1b1fbcdda31b4c0
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-15 09:56:40 +0200

    1 commit

--- app/split_integer.py
+++ app/split_integer.py
@@ -6,9 +6,3 @@ def split_integer(value: int, number_of_parts: int) -> list:
         value -= next_number
     return parts
 
-# print(split_integer(1,3))
-# print(split_integer(10,3))
-# print(split_integer(8, 1), " == [8]")
-# print(split_integer(6, 2), " == [3, 3]")
-# print(split_integer(17, 4), " == [4, 4, 4, 5]")
-# print(split_integer(32, 6), " == [5, 5, 5, 5, 6, 6]")
