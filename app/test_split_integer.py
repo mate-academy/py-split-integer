1b66f8b657a0596312be17d7d5c6ceccba3f861c
Ostap <anastasiia@Anastasiias-Air.Dlink>
2023-02-22 17:30:46 +0200

    fixed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -10,7 +10,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert len(split_integer(16, 1)) == 1
+    assert split_integer(16, 1) == [16]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
