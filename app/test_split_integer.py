ee76e98d6c644f48276af194c6697597f56d7b12
Yulia <Mala>
2023-02-23 14:37:09 +0200

    fixed what was already working

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,16 +1,21 @@
-import pytest
 from app.split_integer import split_integer
 
 
-@pytest.mark.parametrize(
-    "number,parts,result",
-    [
-        pytest.param(9, 3, [3, 3, 3], id="equal parts expected if possible"),
-        pytest.param(5, 1, [5], id="same value expected when part = 1"),
-        pytest.param(11, 3, [3, 4, 4], id="sorted list expected"),
-        pytest.param(2, 3, [0, 1, 1], id="should add zeros if parts > value"),
-    ]
-)
-def test_split_int(number: int, parts: int, result: list) -> None:
-    assert split_integer(number, parts) == result, (
-        f"expected {result} when number is {number} and parts is {parts}")
+def test_sum_of_the_parts_should_be_equal_to_value() -> None:
+    assert sum(split_integer(9, 5)) == 9, "parts sum should be equal to value"
+
+
+def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
+    assert split_integer(9, 3) == [3, 3, 3], "equal parts expected if possible"
+
+
+def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
+    assert split_integer(5, 1) == [5], "same value expected when part = 1"
+
+
+def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
+    assert split_integer(11, 3) == [3, 4, 4], "sorted list expected"
+
+
+def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
+    assert split_integer(2, 3) == [0, 1, 1], "must add zeros if parts > value"
