a2136bba12838abfa7ab1a557a4da07a26a7c38b
Anna Blahitka <anettaabl@gmail.com>
2023-02-28 12:21:12 +0300

    Corected

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert split_integer(8, 1) == [8]
+    assert sum([2, 2, 2, 3]) == 9
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -10,11 +10,11 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(17, 4) == [4, 4, 4, 5]
+    assert split_integer(20, 4) == [5] + [5] + [5] + [5]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    assert split_integer(32, 6) == sorted([6, 5, 5, 6, 5, 5])
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
