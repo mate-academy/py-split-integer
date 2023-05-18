3140422a16b4e4084264e146aeb019bf13f71ffb
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-05-18 11:26:55 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,35 +2,29 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 6
-    sum_of_parts = 2
-    parts = [3, 3]
-    assert sum(parts) == value
+    goals = split_integer(6, 2)
+    assert sum(goals) == 6
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 6
-    number_of_parts = 2
+    goals = split_integer(6, 2)
     parts = [3, 3]
-    assert split_integer(value, number_of_parts) == parts
+    assert goals == parts
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 6
-    number_of_parts = 1
+    goals = split_integer(6, 1)
     parts = [6]
-    assert split_integer(value, number_of_parts) == parts
+    assert goals == parts
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 32
-    number_of_parts = 6
-    parts = split_integer(value, number_of_parts)
-    assert parts == sorted(parts)
+    goals = split_integer(32, 6)
+    parts = [5, 5, 5, 5, 6, 6]
+    assert goals == parts
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 3
-    number_of_parts = 6
+    goals = split_integer(3, 6)
     parts = [0, 0, 0, 1, 1, 1]
-    assert split_integer(value, number_of_parts) == parts
+    assert goals == parts
