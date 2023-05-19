f8683dbb71d80de5fea0aeb7b0b4a774682525ed
Vitalii2906 <fahivchik1@gmail.com>
2023-05-19 12:39:38 +0300

    Solution5

--- app/split_integer.py
+++ app/split_integer.py
@@ -4,5 +4,4 @@ def split_integer(value: int, number_of_parts: int) -> list:
         next_number = value // parts_left
         parts.append(value // parts_left)
         value -= next_number
-    parts.sort()
     return parts
