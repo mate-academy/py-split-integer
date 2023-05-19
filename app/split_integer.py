198702e4310c0576fdb0244ee330aa868ac1a261
Vitalii2906 <fahivchik1@gmail.com>
2023-05-19 12:06:00 +0300

    Solution3

--- app/split_integer.py
+++ app/split_integer.py
@@ -3,4 +3,8 @@ def split_integer(value: int, number_of_parts: int) -> list:
     remainder = value % number_of_parts
     for i in range(remainder):
         parts[i] += 1
+    parts.sort()
     return parts
+
+
+print(split_integer(5, 6))
