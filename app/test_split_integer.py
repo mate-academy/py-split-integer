63c936798150aa6967c74ae58635fd0f956797fc
o.makieieva <oksana.zhornyk@gmail.com>
2023-05-20 18:39:54 +0200

    py-split-integer-v1

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,79 @@
+import pytest
+
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+@pytest.mark.parametrize(
+    "value,number_of_parts",
+    [
+        (8, 1),
+        (6, 2),
+        (17, 4),
+        (32, 6)
+    ]
+)
+def test_sum_of_the_parts_should_be_equal_to_value(
+        value: int, number_of_parts: int
+) -> None:
+    assert sum(split_integer(value, number_of_parts)) == value
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+@pytest.mark.parametrize(
+    "value,number_of_parts,result",
+    [
+        (8, 1, [8]),
+        (6, 2, [3, 3]),
+        (16, 4, [4, 4, 4, 4]),
+        (36, 6, [6, 6, 6, 6, 6, 6])
+    ]
+)
+def test_should_split_into_equal_parts_when_value_divisible_by_parts(
+        value: int, number_of_parts: int, result: list
+) -> None:
+    assert split_integer(value, number_of_parts) == result
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+@pytest.mark.parametrize(
+    "value,number_of_parts",
+    [
+        (8, 1),
+        (6, 1),
+        (17, 1),
+        (32, 1)
+    ]
+)
+def test_should_return_part_equals_to_value_when_split_into_one_part(
+        value: int, number_of_parts: int
+) -> None:
+    assert split_integer(value, number_of_parts) == [value]
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+@pytest.mark.parametrize(
+    "value,number_of_parts",
+    [
+        (8, 1),
+        (6, 2),
+        (17, 4),
+        (32, 6)
+    ]
+)
+def test_parts_should_be_sorted_when_they_are_not_equal(
+        value: int, number_of_parts: int
+) -> None:
+    assert (split_integer(value, number_of_parts) == sorted(
+        split_integer(value, number_of_parts)))
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+@pytest.mark.parametrize(
+    "value,number_of_parts,result",
+    [
+        (3, 5, [0, 0, 1, 1, 1]),
+        (2, 3, [0, 1, 1]),
+        (2, 6, [0, 0, 0, 0, 1, 1]),
+        (0, 1, [0])
+    ]
+)
+def test_should_add_zeros_when_value_is_less_than_number_of_parts(
+        value: int, number_of_parts: int, result: list
+) -> None:
+    assert split_integer(value, number_of_parts) == result
