434927787c621ee71f538cb91796f69de93e2ef7
Ivan <ivan.korshunov.1997@gmail.com>
2023-05-20 15:49:46 +0300

    remove redundant variables

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,31 +2,25 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    parts = split_integer(50, 6)
 
-    assert sum(parts) == 50
+    assert sum(split_integer(50, 6)) == 50
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    parts = split_integer(50, 5)
 
-    assert len(set(parts)) == 1
+    assert len(set(split_integer(50, 5))) == 1
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    parts = split_integer(50, 1)
 
-    assert parts[0] == 50
+    assert split_integer(50, 1)[0] == 50
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    parts = split_integer(50, 6)
-    parts_sorted = sorted(parts)
 
-    assert parts == parts_sorted
+    assert split_integer(50, 6) == sorted(split_integer(50, 6))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    parts = split_integer(4, 6)
 
-    assert parts == [0, 0, 1, 1, 1, 1]
+    assert split_integer(4, 6) == [0, 0, 1, 1, 1, 1]
