cc52a2065c64bc1bd8665d931c1dbefdf3917904
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-05-20 15:10:53 +0300

    Fix Py-split_integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(11, 3)) == sum([3, 4, 4])
+    assert sum(split_integer(11, 3)) == 11
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
