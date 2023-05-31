de33b9baecad70b751bcd38a9609af3165456ade
S-SMTN <cven28@gmail.com>
2023-05-31 19:44:51 +0300

    implemented test_split_integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -19,9 +19,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 5
-    parts = 7
-    splitted_integer = split_integer(value, parts)
-    assert (
-        splitted_integer[:parts - value] == [0 for _ in range(parts - value)]
-    )
+    assert split_integer(3, 5) == [0, 0, 1, 1, 1]
