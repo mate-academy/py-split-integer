1b1bbfa2381e8f19d804f9369d823dd75612857b
Glieb <gleblitvinenko3007@gmail.com>
2023-06-01 21:35:07 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,30 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    input_data = (32, 6)
+    test_res = split_integer(input_data[0], input_data[1])
+    assert sum(test_res) == input_data[0]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    input_data = (36, 6)
+    test_res = split_integer(input_data[0], input_data[1])
+    assert input_data[0] // input_data[1] == len(test_res)
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    input_data = (36, 1)
+    test_res = split_integer(input_data[0], input_data[1])
+    assert len(test_res) == 1
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    input_data = (32, 6)
+    test_res = split_integer(input_data[0], input_data[1])
+    assert test_res == sorted(test_res)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    input_data = (1, 6)
+    test_res = split_integer(input_data[0], input_data[1])
+    assert test_res.count(0) == input_data[1] - input_data[0]
