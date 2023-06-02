953591d920b349c357df0cc59300c9981abd4211
Glieb <gleblitvinenko3007@gmail.com>
2023-06-02 07:59:26 +0300

    Fixed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -18,4 +18,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(4,6) == [0, 0, 1, 1, 1, 1]
+    assert split_integer(4, 6) == [0, 0, 1, 1, 1, 1]
