cf4eba2138cc7434cba3920bb5f031534cd0109e
Soloveyk1n <swain.volt@gmail.com>
2023-06-08 13:15:16 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,27 @@
 from app.split_integer import split_integer
+import pytest
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    parts = split_integer(1, 1)
+    assert parts == [1]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    parts = split_integer(6, 2)
+    assert parts == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    parts = split_integer(8, 0)
+    assert parts == []
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    parts = split_integer(17, 4)
+    assert parts == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    parts = split_integer(2, 4)
+    assert parts == [0, 0, 1, 1]
