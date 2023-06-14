781578e35d09333fe4fe26acbd1277bb74f9b375
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-14 18:36:21 +0300

    Refactored

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -11,7 +11,7 @@ from app.split_integer import split_integer
         (10, 1, [10]),
         (25, 2, [12, 13]),
         (30, 5, [6, 6, 6, 6, 6]),
-        (7, 4, [2, 2, 2, 1]),
+        (7, 4, [1, 2, 2, 2]),
     ]
 )
 def test_sum_of_the_parts_should_be_equal_to_value(
@@ -19,44 +19,34 @@ def test_sum_of_the_parts_should_be_equal_to_value(
     number_of_parts: int,
     expected_parts: list[int]
 ) -> None:
-    parts = split_integer(value, number_of_parts)
-    assert sum(parts) == value
+    assert split_integer(value, number_of_parts) == expected_parts
 
 
 @pytest.mark.parametrize(
-    "value, number_of_parts",
+    "value, number_of_parts, expected_part_value",
     [
-        (10, 2),
-        (15, 3),
-        (20, 4),
-        (25, 5),
+        (10, 2, 5),
+        (15, 3, 5),
+        (20, 4, 5),
+        (25, 5, 5),
     ]
 )
 def test_should_split_into_equal_parts_when_value_divisible_by_parts(
     value: int,
-    number_of_parts: int
+    number_of_parts: int,
+    expected_part_value: int
 ) -> None:
     parts = split_integer(value, number_of_parts)
-    expected_part_value = value // number_of_parts
     assert all(part == expected_part_value for part in parts)
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 10
-    number_of_parts = 1
-    parts = split_integer(value, number_of_parts)
-    assert parts == [value]
+    assert split_integer(10, 1) == [10]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 25
-    number_of_parts = 2
-    parts = split_integer(value, number_of_parts)
-    assert parts == [12, 13]
+    assert split_integer(25, 2) == [12, 13]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 4
-    number_of_parts = 5
-    parts = split_integer(value, number_of_parts)
-    assert parts == [0, 1, 1, 1, 1]
+    assert split_integer(4, 5) == [0, 1, 1, 1, 1]
