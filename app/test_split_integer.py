4ba31fadca8d7047c078fbf46804cc49bfe48f0b
Yaryk Konoplyov <kannabis252@gmail.com>
2023-06-15 13:11:17 +0300

    Unnecessary brackets removed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert (sum(split_integer(32, 6)) == 32)
+    assert sum(split_integer(32, 6)) == 32
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert (len(set(split_integer(36, 6))) == 1)
+    assert len(set(split_integer(36, 6))) == 1
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert (sum(split_integer(8, 1)) == 8)
+    assert sum(split_integer(8, 1)) == 8
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert (split_integer(32, 6) == sorted(split_integer(32, 6)))
+    assert split_integer(32, 6) == sorted(split_integer(32, 6))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert (split_integer(1, 3) == [0, 0, 1])
+    assert split_integer(1, 3) == [0, 0, 1]
