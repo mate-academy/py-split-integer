5c6ecacb68db9eca2dfe18ef05633a81a095a679
Yaryk Konoplyov <kannabis252@gmail.com>
2023-06-15 13:34:40 +0300

    Tests for splitting into one part fixed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -10,7 +10,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert sum(split_integer(8, 1)) == 8
+    assert len(split_integer(8, 1)) == 1
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
