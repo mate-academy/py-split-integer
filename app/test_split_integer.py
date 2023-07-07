ec3f94615a0c246b7f7175205e48d4e3fdde53c9
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-07-07 10:42:57 +0300

    test_secont_sol

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -7,8 +7,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(17, 4)
-    assert len(result) == 4
+    assert split_integer(8, 4) == [2, 2, 2, 2]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -17,11 +16,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(32, 6)
-    assert result == [5, 5, 5, 5, 6, 6]
+    assert split_integer(17, 4) == sorted(split_integer(17, 4))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(2, 4)
-    add__zero_result =  result[:2]
-    assert add__zero_result == [0, 0]
+    assert split_integer(3, 6) == [0, 0, 0, 1, 1, 1]
