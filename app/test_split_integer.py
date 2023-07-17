35fda97ab7b52566871432ea7a8b556a5aa274e9
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2023-07-17 15:30:58 +0200

    Solutionstop

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,33 @@
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+# sum of the parts should be equal to value
+def test_check_sum_of_the_parts() -> None:
+    expected_parts = split_integer(20, 4)
+    assert sum(expected_parts) == 20
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+# should split into equal parts when value divisible by parts
+def test_split_into_equal_parts() -> None:
+    expected_parts = split_integer(30, 5)
+    assert all(
+        part == 30 // 5 for part in expected_parts)
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+# should return part equals to value when split into one part
+def test_split_to_single_part() -> None:
+    expected_parts = split_integer(8, 1)
+    assert expected_parts == [8]
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+# parts should be sorted when they are not equal
+def test_sort_non_equal_parts() -> None:
+    expected_parts = split_integer(17, 4)
+    sorted_parts = sorted(expected_parts)
+    assert expected_parts == sorted_parts
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+# should add zeros when value is less than number of parts
+def test_add_zeros_for_small_value() -> None:
+    expected_parts = split_integer(3, 4)
+    assert expected_parts == [0, 1, 1, 1]
