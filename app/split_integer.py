fdb9a8227165b4a6879145626435ec88b378ac60
Artem Putsenko <artempucenko3@gmail.com>
2023-07-18 20:20:55 +0300

    '1'

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,7 +1,7 @@
-def split_integer(value: int, number_of_parts: int) -> list:
-    parts = []
-    for parts_left in range(number_of_parts, 0, -1):
-        next_number = value // parts_left
-        parts.append(value // parts_left)
-        value -= next_number
-    return parts
+from typing import List
+
+
+def split_integer(value: int, number_of_parts: int) -> List[int]:
+    quotient, remainder = divmod(value, number_of_parts)
+    return [quotient + 1] * remainder + [quotient] *\
+        (number_of_parts - remainder)
--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,31 @@
-from app.split_integer import split_integer
+from .split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+def test_difference_between_max_and_min() -> None:
+    num_parts = 5
+    for value in [10, 15, 20, 25, 30]:
+        result = split_integer(value, num_parts)
+        assert max(result) - min(result) <= 1
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+def test_array_sorted_ascending() -> None:
+    num_parts = 5
+    for value in [12, 18, 22, 29, 33]:
+        result = split_integer(value, num_parts)
+        assert result == sorted(result)
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+def test_single_element_array() -> None:
+    assert split_integer(8, 1) == [8]
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+def test_two_elements_array() -> None:
+    assert split_integer(6, 2) == [3, 3]
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+def test_four_elements_array() -> None:
+    assert split_integer(17, 4) == [4, 4, 4, 5]
+
+
+def test_six_elements_array() -> None:
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
