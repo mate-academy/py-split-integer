823e80f632d280afd3b6962cc211a7775c7b9cef
Vlad Kleshko <vkoesko@gmail.com>
2023-07-20 18:28:23 +0300

    solution of py-split-integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,35 +2,25 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 17
-    number_of_parts = 4
 
-    assert sum(split_integer(value, number_of_parts)) == 17
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 6
-    number_of_parts = 2
 
-    assert split_integer(value, number_of_parts) == [3, 3]
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 8
-    number_of_parts = 1
 
-    assert split_integer(value, number_of_parts) == [8]
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 32
-    number_of_parts = 6
 
-    assert split_integer(value, number_of_parts) == [5, 5, 5, 5, 6, 6]
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 3
-    number_of_parts = 4
 
-    assert split_integer(value, number_of_parts) == [0, 1, 1, 1]
+    assert split_integer(3, 4) == [0, 1, 1, 1]
