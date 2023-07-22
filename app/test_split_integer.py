2d6a29e88cc74cbd4e36c8f145deaf11e6a2926e
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-07-22 14:36:31 +0300

    flake8 fix

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -5,13 +5,11 @@ def test_sum_of_the_parts_equal_to_value() -> None:
     assert sum(split_integer(15, 3)) == 15
 
 
-def test_split_equal_when_value_divisible_by_parts(
-        ) -> None:
+def test_split_equal_when_value_divisible_by_parts() -> None:
     assert len(split_integer(10, 2)) == 2
 
 
-def test_return_part_equals_to_value_when_split_into_one_part(
-        ) -> None:
+def test_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(10, 1) == [10]
 
 
@@ -19,6 +17,5 @@ def test_parts_sorted_when_they_are_not_equal() -> None:
     assert split_integer(11, 3) == sorted(split_integer(11, 3))
 
 
-def test_add_zeros_when_value_is_less_than_number_of_parts(
-        ) -> None:
+def test_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(1, 3) == [0, 0, 1]
