7317fa9f27cc587a6aa740de685b2e8206fb44f5
Artem Putsenko <artempucenko3@gmail.com>
2023-07-24 18:09:01 +0300

    '1'

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,12 +1,7 @@
 def split_integer(value: int, number_of_parts: int) -> list:
-    base_part = value // number_of_parts
-    remaining = value % number_of_parts
-
-    parts = [base_part] * number_of_parts
-
-    for i in range(remaining):
-        parts[i] += 1
-
-    parts.sort()
-
+    parts = []
+    for parts_left in range(number_of_parts, 0, -1):
+        next_number = value // parts_left
+        parts.append(value // parts_left)
+        value -= next_number
     return parts
