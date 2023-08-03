9c3ef569420d52fba53792c55fa7ec0d4f59ba53
Viktor Deyneka <3lobites@gmail.com>
2023-08-03 20:00:19 +0300

    1

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -3,43 +3,24 @@ from app.split_integer import split_integer
 
 # тестовая сумма частей должна быть равна значению
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result_function = split_integer(17, 4)
-    actual = sum(result_function)
-    expected = 17
-
-    assert actual == expected, f"{actual} is not equal to {expected}"
+    assert split_integer(8, 2) == [4, 4]
 
 
 # тест должен быть разделен на равные части, когда значение делится на части
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    expected = [4, 4, 4, 5]
-
-    assert split_integer(17, 4) == expected, f"{split_integer(17, 4)} is not equal to {expected}"
+    assert split_integer(20, 5) == [4, 4, 4, 4, 4]
 
 
 # тест должен возвращать часть, равную значению, при разделении на одну часть
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    result_function = split_integer(8, 1)
-    actual = result_function
-    expected = [8]
-
-    assert actual == expected, f"{actual} is not equal to {expected}"
+    assert  split_integer(8, 1) == [8]
 
 
 # тестовые части должны быть отсортированы, если они не равны
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result_function = split_integer(17, 4)
-    actual = result_function
-    expected = result_function.copy()
-    expected.sort()
-
-    assert actual == expected, f"{actual} is not equal to {expected}"
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 # тест должен добавлять нули, когда значение меньше количества частей
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result_function = split_integer(1, 4)
-    actual = result_function
-    expected = [0, 0, 0, 1]
-
-    assert actual == expected, f"{actual} is not equal to {expected}"
+    assert split_integer(1, 4) == [0, 0, 0, 1]
