b5ae2ec19bf625f7a010b7cb4b70cb0c5e511c78
Nazarii Schcur <nazae530@gmail.com>
2023-08-04 12:16:02 +0300

    fix bag

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -10,8 +10,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     value = 25
     number_of_parts = 5
-    assert (all(x == split_integer(value, number_of_parts)[0]
-                for x in split_integer(value, number_of_parts)))
+    assert len(set(split_integer(value, number_of_parts))) == 1
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
