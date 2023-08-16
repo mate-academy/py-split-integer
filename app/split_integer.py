641f27e9197e84b7b25b54ef040bab23a9a772d2
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-17 00:29:49 +0300

    Solution

--- app/split_integer.py
+++ app/split_integer.py
@@ -2,6 +2,9 @@ def split_integer(value: int, number_of_parts: int) -> list:
     parts = []
     for parts_left in range(number_of_parts, 0, -1):
         next_number = value // parts_left
-        parts.append(value // parts_left)
+        parts.append(next_number)
         value -= next_number
     return parts
+
+
+print(split_integer(3, 6) == [0, 0, 0, 1, 1, 1])
--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,45 @@
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+class TestSplitInteger:
+    def test_sum_of_the_parts_should_be_equal_to_value(self) -> None:
+        assert (
+            sum(split_integer(32, 6)) == 32
+        ), "Sum of parts should be equal to value"
 
+    def test_should_split_into_equal_parts_when_value_divisible_by_parts(
+        self,
+    ) -> None:
+        assert split_integer(6, 2) == [
+            3,
+            3,
+        ], "Should_split_into_equal_parts_when_value_divisible_by_parts"
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    def test_should_return_part_equals_to_value_when_split_into_one_part(
+        self,
+    ) -> None:
+        assert split_integer(8, 1) == [
+            8
+        ], "should_return_part_equals_to_value_when_split_into_one_part"
 
+    def test_parts_should_be_sorted_when_they_are_not_equal(self) -> None:
+        assert split_integer(32, 6) == [
+            5,
+            5,
+            5,
+            5,
+            6,
+            6,
+        ], "parts_should_be_sorted_when_they_are_not_equal"
 
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
-
-
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
-
-
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    def test_should_add_zeros_when_value_is_less_than_number_of_parts(
+        self,
+    ) -> None:
+        assert split_integer(3, 6) == [
+            0,
+            0,
+            0,
+            1,
+            1,
+            1,
+        ], "should_add_zeros_when_value_is_less_than_number_of_parts"
