e49e02c3b6e6f1ab3cdf0ead5e5404db98ed2594
Yeva <yevabrookman@gmail.com>
2023-08-22 13:33:43 +0300

    Sorting separated of other tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -22,12 +22,6 @@ class TestSplitInteger:
                 "when split into one part",
             ),
             pytest.param(
-                17,
-                4,
-                [4, 4, 4, 5],
-                id="test parts should be sorted when they are not equal",
-            ),
-            pytest.param(
                 4,
                 6,
                 [0, 0, 1, 1, 1, 1],
@@ -42,6 +36,13 @@ class TestSplitInteger:
         result = split_integer(value, number_of_parts)
 
         assert result == expected_result
+
+    def test_parts_should_be_sorted_when_they_are_not_equal(self):
+        value = 17
+        number_of_parts = 4
+        result = split_integer(value, number_of_parts)
+
+        assert sorted(result) == result
 
     def test_sum_of_the_parts_should_be_equal_to_value(self) -> None:
         value = 17
