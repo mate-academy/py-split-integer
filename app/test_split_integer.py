1e3036f3d4e5b303a4a792c79d1b0417f2a5dea1
Taras Borshch <tarasborshch4@gmail.com>
2023-08-23 21:00:46 +0300

    fixed error

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -10,7 +10,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(10, 1) == 10
+    assert split_integer(10, 1)[0] == 10
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
