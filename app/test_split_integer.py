c7fe203a45b0b2363a55097c6042cc35f13f3a81
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-08-25 18:37:46 +0300

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,48 @@
+import pytest
+
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+class TestSplitInteger:
+    def test_sum_of_the_parts_should_be_equal_to_value(self) -> None:
+        assert sum(split_integer(20, 4)) == 20, ("sum of the parts should be "
+                                                 "equal to the original value")
 
-
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
-
-
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
-
-
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
-
-
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    @pytest.mark.parametrize(
+        "value, number_of_parts, result",
+        [
+            pytest.param(
+                20,
+                5,
+                [4, 4, 4, 4, 4],
+                id=("should split into equal parts "
+                    "when value divisible by parts")
+            ),
+            pytest.param(
+                5,
+                1,
+                [5],
+                id=("should return part equals to value "
+                    "when split into one part")
+            ),
+            pytest.param(
+                30,
+                4,
+                [7, 7, 8, 8],
+                id="parts should be sorted when they are not equal"
+            ),
+            pytest.param(
+                5,
+                8,
+                [0, 0, 0, 1, 1, 1, 1, 1],
+                id="should add zeros when value is less than number of parts"
+            )
+        ]
+    )
+    def test_split_integer_correctly(
+        self,
+        value: int,
+        number_of_parts: int,
+        result: list
+    ) -> None:
+        assert split_integer(value, number_of_parts) == result
