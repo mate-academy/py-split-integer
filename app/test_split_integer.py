2d7228d45eeb81a65d3425782b8ca086b9ca37e7
CaCtUs1k <denischernish19012004@gmail.com>
2023-08-25 16:28:41 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -3,29 +3,29 @@ from app.split_integer import split_integer
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert (
-        sum(split_integer(41, 5)) == 41,
-        "Sum of the parts should be equal to value")
+        sum(split_integer(41, 5)) == 41
+    ), "Sum of the parts should be equal to value"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert (
-        split_integer(40, 8) == [5, 5, 5, 5, 5, 5, 5, 5],
-        "Function should split into equal parts when value divisible by parts")
+        split_integer(40, 8) == [5, 5, 5, 5, 5, 5, 5, 5]
+    ), "Function should split into equal parts when value divisible by parts"
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert (
-        split_integer(21, 1) == [21],
-        "Function should return part equals to value when split into one part")
+        split_integer(21, 1) == [21]
+    ), "Function should return part equals to value when split into one part"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert (
-        split_integer(7, 3) == [2, 2, 3],
-        "Parts should be sorted when they are not equal")
+        split_integer(7, 3) == [2, 2, 3]
+    ), "Parts should be sorted when they are not equal"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert (
-        split_integer(4, 5) == [1, 1, 1, 1, 0],
-        "Function should add zeros when value is less than number of parts")
+        split_integer(4, 5) == [0, 1, 1, 1, 1]
+    ), "Function should add zeros when value is less than number of parts"
