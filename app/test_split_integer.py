6ec6cc35facb227c2e9091fcaf8951d0b8aa02de
oreny <orenycdota@gmail.com>
2023-08-25 09:51:08 +0300

    Implemented 5 tests according to the task

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,31 @@
+import copy
+
 from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    goals = split_integer(5, 3)
+    assert sum(goals) == 5
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    goals = split_integer(9, 3)
+    assert goals[0] == goals[1] == goals[2]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    goals = split_integer(4, 1)
+    assert goals[0] == 4
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    goals = split_integer(32, 6)
+    sorted_goals = copy.copy(goals)
+    sorted_goals.sort()
+    assert goals == sorted_goals
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    goals = split_integer(1, 2)
+    required_list = [0, 1]
+    assert goals == required_list
