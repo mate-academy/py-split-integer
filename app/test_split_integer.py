ebd6d1f27a20bc1a137b239aec4623dd018f689d
oreny <orenycdota@gmail.com>
2023-08-25 12:22:48 +0300

    Read checklist

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -4,8 +4,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    goals = split_integer(5, 3)
-    assert sum(goals) == 5
+    assert sum(split_integer(5, 3)) == 5
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -14,8 +13,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    goals = split_integer(4, 1)
-    assert goals[0] == 4
+    assert split_integer(4, 1)[0] == 4
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -26,6 +24,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    goals = split_integer(1, 2)
-    required_list = [0, 1]
-    assert goals == required_list
+    assert split_integer(1, 2) == [0, 1]
