9676fa142b011b5286cd7775aae10e3d1c5f961c
Max Katkalov <makskatkalov@gmail.com>
2023-08-25 07:52:29 +0300

    tests added for main.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,39 @@
+import pytest
+
+
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
-
-
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
-
-
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
-
-
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
-
-
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+@pytest.mark.parametrize(
+    "value, number_of_parts, result_list",
+    (
+        pytest.param(
+            8,
+            1,
+            [8],
+            id="should return part equals to value when split into one part",
+        ),
+        pytest.param(
+            17,
+            4,
+            [4, 4, 4, 5],
+            id="parts should be sorted when they are not equal",
+        ),
+        pytest.param(
+            5,
+            6,
+            [0, 1, 1, 1, 1, 1],
+            id="should add zeros when value is less than number of parts",
+        ),
+        pytest.param(
+            36,
+            6,
+            [6, 6, 6, 6, 6, 6],
+            id="should split into equal parts when value divisible by parts",
+        ),
+    ),
+)
+def test_correct_work_of_split_integer(
+    value: int, number_of_parts: int, result_list: list[int]
+) -> None:
+    assert split_integer(value, number_of_parts) == result_list
