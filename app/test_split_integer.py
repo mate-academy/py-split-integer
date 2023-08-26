b1d40f2c74dd7c82ed106a5d6274bc07445dd069
struhanets <posseydon87@gmail.com>
2023-08-26 15:00:44 +0200

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,47 @@
+import pytest
+
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+@pytest.mark.parametrize("value, number_of_parts", [(6, 2)])
+def test_sum_of_the_parts_should_be_equal_to_value(value, number_of_parts) -> None:
+    parts = split_integer(value, number_of_parts)
+    assert sum(parts) == value
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+@pytest.mark.parametrize("value, number_of_parts", [(32, 6)])
+def test_should_split_into_equal_parts_when_value_divisible_by_parts(value, number_of_parts) -> None:
+    parts = split_integer(value, number_of_parts)
+    assert len(parts) == number_of_parts
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+@pytest.mark.parametrize("value, number_of_parts", [(8, 1)])
+def test_should_return_part_equals_to_value_when_split_into_one_part(value, number_of_parts) -> None:
+    parts = split_integer(value, number_of_parts)
+    if len(parts) == 1:
+        assert parts[0] == value
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+@pytest.mark.parametrize("value, number_of_parts", [(17, 4)])
+def test_parts_should_be_sorted_when_they_are_not_equal(
+        value,
+        number_of_parts
+) -> None:
+    parts = split_integer(value, number_of_parts)
+    assert parts == sorted(parts)
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+# @pytest.mark.parametrize(
+#     "value, number_of_parts, expected_res",
+#     [
+#         (8, 1, [8]),
+#     ]
+# )
+# def test_should_add_zeros_when_value_is_less_than_number_of_parts(
+#         value,
+#         number_of_parts,
+#         expected_res
+# ) -> None:
+#     split_integer(value, number_of_parts)
+#     if number_of_parts is None:
+#         assert number_of_parts == 0
