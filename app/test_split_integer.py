2a09077603cf01e250e2fdaded7ce56a9aad877d
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-08-26 16:00:25 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -38,9 +38,5 @@ class TestSplitInteger:
         assert split_integer(value, number_of_parts) == expected_result
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(32, 6)) == sum([5, 5, 5, 5, 6, 6])
-
-
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert sorted(split_integer(26, 4)) == [6, 6, 7, 7]
