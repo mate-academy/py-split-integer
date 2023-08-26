7448cae2a036ec99dc9636930f8b4e31b6f22b8e
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-08-26 22:26:33 +0300

    solution... I hope

--- app/support.py
+++ app/support.py
@@ -4,8 +4,8 @@ from math import ceil
 def splitting_list_into_two_equal_parts(input_list: list) -> tuple:
     if len(input_list) >= 1:
         middle = len(input_list) / 2
-        right_side = round(middle)
+        right_side = int(middle)
         left_side = ceil(middle) if isinstance(middle, float) else right_side
         return input_list[:left_side], input_list[right_side:]
-    raise Exception(("You use function "
+    raise TypeError(("You use function "
                      "splitting_list_into_two_equal_parts incorrect!"))
--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -8,12 +8,12 @@ def test_sum_of_the_parts_equal_to_value() -> None:
 
 def test_split_equal_when_value_divisible_by_parts() -> None:
     left, right = splitting_list_into_two_equal_parts(split_integer(12, 4))
-    assert left == right
+    assert sum(left) == sum(right)
 
 
 def test_split_value_on_different_parts() -> None:
     left, right = splitting_list_into_two_equal_parts(split_integer(17, 5))
-    assert left < right
+    assert sum(left) < sum(right)
 
 
 def test_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -21,12 +21,6 @@ def test_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_sorted_when_they_are_not_equal() -> None:
-    """
-    With my solution this test is redundant.
-    My test "split_value_on_different_parts" checks it also.
-    At least it seems so to me. :)
-    """
-
     assert split_integer(11, 3) == sorted(split_integer(11, 3))
 
 
@@ -38,6 +32,5 @@ def test_is_split_integer_length_correct() -> None:
     assert len(split_integer(100, 64)) == 64
 
 
-def test_split_when_just_last_one_number_biggest() -> None:
-    element = split_integer(10, 3)
-    assert element[-1] == min(element) + 1
+def test_when_number_of_parts_zero() -> None:
+    assert split_integer(10, 0) == []
