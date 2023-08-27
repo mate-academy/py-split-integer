f627b07ebf0cf68ec9f0412a63e7714d8d744921
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-08-27 13:43:07 +0300

    simplified tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,30 +1,23 @@
 from app.split_integer import split_integer
-from app.support import splitting_list_into_two_equal_parts
 
 
-def test_sum_of_the_parts_equal_to_value() -> None:
+def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(15, 3)) == 15
 
 
-def test_split_equal_when_value_divisible_by_parts() -> None:
-    left, right = splitting_list_into_two_equal_parts(split_integer(12, 4))
-    assert sum(left) == sum(right)
+def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
+    assert split_integer(40, 8) == [5, 5, 5, 5, 5, 5, 5, 5]
 
 
-def test_split_value_on_different_parts() -> None:
-    left, right = splitting_list_into_two_equal_parts(split_integer(17, 5))
-    assert sum(left) < sum(right)
-
-
-def test_return_part_equals_to_value_when_split_into_one_part() -> None:
+def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(10, 1) == [10]
 
 
-def test_parts_sorted_when_they_are_not_equal() -> None:
+def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert split_integer(11, 3) == sorted(split_integer(11, 3))
 
 
-def test_add_zeros_when_value_is_less_than_number_of_parts() -> None:
+def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(1, 3) == [0, 0, 1]
 
 
