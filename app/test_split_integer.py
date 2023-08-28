bb419dd4c2fcc67b18c0ece4cdc8055d313ad045
oreny <orenycdota@gmail.com>
2023-08-28 20:24:54 +0300

    Applied simplifies

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -8,8 +8,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    goals = split_integer(9, 3)
-    assert goals[0] == goals[1] == goals[2]
+    assert split_integer(9, 3) == [3, 3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -17,10 +16,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    goals = split_integer(32, 6)
-    sorted_goals = copy.copy(goals)
-    sorted_goals.sort()
-    assert goals == sorted_goals
+    sorted_goals = split_integer(32, 6)
+    assert split_integer(32, 6) == sorted_goals
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
