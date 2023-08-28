5b37b317bc66b9650b5f8fbb48b2b0e682322057
ivan <ivan@isba.local>
2023-08-28 16:54:07 +0300

    Fix

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert len(split_integer(32, 6)) == 6
+    assert sum(split_integer(32, 6)) == 6
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
