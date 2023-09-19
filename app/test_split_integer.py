13d41d2fca2458b91ace4a30944620765d7557c0
Andriy_Shumskyy <andshumskyy@gmail.com>
2023-09-19 15:30:20 +0300

    solution2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,35 +2,25 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 10
-    number_of_parts = 3
-    parts = split_integer(value, number_of_parts)
-    assert sum(parts) == value
+    parts = split_integer(10, 3)
+    assert sum(parts) == 10
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 15
-    number_of_parts = 3
-    parts = split_integer(value, number_of_parts)
-    assert all(part == value // number_of_parts for part in parts)
+    actual_parts = split_integer(15, 3)
+    assert actual_parts == [5, 5, 5]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 15
-    number_of_parts = 1
-    parts = split_integer(value, number_of_parts)
-    assert parts[0] == value
+    parts = split_integer(15, 1)
+    assert parts[0] == 15
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 15
-    number_of_parts = 4
-    parts = split_integer(value, number_of_parts)
+    parts = split_integer(15, 4)
     assert parts == [3, 4, 4, 4]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 2
-    number_of_parts = 4
-    parts = split_integer(value, number_of_parts)
+    parts = split_integer(2, 4)
     assert parts == [0, 0, 1, 1]
