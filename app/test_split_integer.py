baec96ffe3686fee47aa945227813bba17389a19
Dmytro Tatarinov <nsd7night@gmail.com>
2023-09-29 20:55:49 +0300

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,8 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    parts = split_integer(42, 7)
-    assert all(part == parts[0] for part in parts)
+    assert split_integer(42, 7) == [6, 6, 6, 6, 6, 6, 6]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
