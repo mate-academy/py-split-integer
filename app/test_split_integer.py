17c6889898067e365e4d4c210a4eaeeb8961a160
Denys <den.prislipskyi@gmail.com>
2023-10-19 16:49:54 +0200

    fix test

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,8 +2,9 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    total = split_integer(6, 2)
-    assert sum(total) == 6
+    # total = split_integer(6, 2)
+    # assert sum(total) == 6
+    assert sum(split_integer(6, 2)) == 6
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
