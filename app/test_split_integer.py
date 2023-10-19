523d2efeaa1baa2284e2624f5102a4fcba3db63a
Denys <den.prislipskyi@gmail.com>
2023-10-19 08:49:19 +0200

    fix test function

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -11,8 +11,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    result = split_integer(17, 4)
-    assert len(result) == 4 and result[0] == 4
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -20,4 +19,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    assert split_integer(2, 5) == [0, 0, 0, 1, 1]
