e46a9ea3d5cb6f4f726382362f548365086afd86
Ruslan Misko <miskoruslan6@gmail.com>
2023-10-19 14:37:24 +0300

    Fix test

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,8 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result = sum(split_integer(19, 4))
-    assert result == 19
+    assert sum(split_integer(19, 4)) == 19
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
