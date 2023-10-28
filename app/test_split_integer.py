0718c91febda09bc24cb1548c2661da52ae42d39
Frezworx <frezworx@gmail.com>
2023-10-28 21:15:49 +0300

    Fix indentation

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,22 +2,23 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(10, 3)) == 10, \
-        "Sum of the parts should be equal to value"
+    assert (
+        sum(split_integer(10, 3)) == 10
+    ), "Sum of the parts should be equal to value"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert (sum(split_integer(6, 3))
-            % len(split_integer(6, 3)) == 0), (
+    parts = split_integer(6, 3)
+    assert sum(parts) % len(parts) == 0, (
         "The sum of the numbers in the list must be divisible by the number"
         " of parts without a remainder "
     )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(5, 1) == [
-        5
-    ], "Should return part equals to value when split into one part"
+    assert (
+        split_integer(5, 1) == [5]
+    ), "Should return part equals to value when split into one part"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -29,8 +30,9 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     result = split_integer(5, 11)
     assert len(result) == 11, "The number of parts must be correct"
-    assert sum(result) == 5, \
-        "The sum of all parts must be equal to the original number"
+    assert (
+        sum(result) == 5
+    ), "The sum of all parts must be equal to the original number"
     assert result.count(0) == 6, (
         "The number of zeros must be equal to the "
         "difference between the number of list parts and the value"
