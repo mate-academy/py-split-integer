431ed7dd2d2e892343d4530ed2707590b3a5434d
Frezworx <frezworx@gmail.com>
2023-10-29 13:05:02 +0200

    Fix unnecessary variables

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -8,17 +8,18 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    parts = split_integer(6, 3)
-    assert sum(parts) % len(parts) == 0, (
+    assert (
+        sum(split_integer(6, 3))
+        % len(split_integer(6, 3)) == 0), (
         "The sum of the numbers in the list must be divisible by the number"
         " of parts without a remainder "
     )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert (
-        split_integer(5, 1) == [5]
-    ), "Should return part equals to value when split into one part"
+    assert split_integer(5, 1) == [
+        5
+    ], "Should return part equals to value when split into one part"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -28,12 +29,13 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(5, 11)
-    assert len(result) == 11, "The number of parts must be correct"
     assert (
-        sum(result) == 5
+        len(split_integer(5, 11)) == 11
+    ), "The number of parts must be correct"
+    assert (
+        sum(split_integer(5, 11)) == 5
     ), "The sum of all parts must be equal to the original number"
-    assert result.count(0) == 6, (
+    assert split_integer(5, 11).count(0) == 6, (
         "The number of zeros must be equal to the "
         "difference between the number of list parts and the value"
     )
