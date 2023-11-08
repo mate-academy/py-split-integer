6e2b670eb87f68b8cb8053dfd08f2a7ba0be1bb3
Andrii Malovanyi <malovaniy.an@gmail.com>
2023-11-08 15:50:10 +0200

    fix assert

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -12,7 +12,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     result = split_integer(8, 1)
-    assert len(result) == 1 and result[0] == 8
+    assert result == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
