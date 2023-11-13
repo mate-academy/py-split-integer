a09c3573af6be7d69e45c5c866b1ebf584d7d6ea
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-11-14 00:56:47 +0200

    Remove all  "result" variable.

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,35 +2,30 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result = split_integer(10, 3)
-    assert sum(result) == 10, (
+    assert sum(split_integer(10, 3)) == 10, (
         "Sum of the parts should be equal to value."
     )
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(16, 4)
-    assert len(set(result)) == 1, (
+    assert len(set(split_integer(16, 4))) == 1, (
         "Should split into equal parts when value divisible by parts."
     )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    result = split_integer(7, 1)
-    assert result == [7], (
+    assert split_integer(7, 1) == [7], (
         "Should return part equals to value when split into one part."
     )
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(18, 5)
-    assert result == sorted(result), (
+    assert split_integer(18, 5) == sorted(split_integer(18, 5)), (
         "Parts should be sorted when they are not equal."
     )
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(2, 5)
-    assert result.count(0) == 3, (
+    assert split_integer(2, 5).count(0) == 3, (
         "Should add zeros when value is less than the number of parts."
     )
