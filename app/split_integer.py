5fd9cf495abd38b3e1b1b63a2e636fa5aa9dbccf
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-11-13 20:04:48 +0200

    Implemented tests for the "split integer" function.

--- app/split_integer.py
+++ app/split_integer.py
@@ -5,10 +5,3 @@ def split_integer(value: int, number_of_parts: int) -> list:
         parts.append(value // parts_left)
         value -= next_number
     return parts
-
-
-a = split_integer(2, 5)
-
-b = all(part == 4 for part in a)
-print(a)
-print(b)
--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -11,21 +11,21 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     result = split_integer(16, 4)
     assert len(set(result)) == 1, (
-        "should split into equal parts when value divisible by parts."
+        "Should split into equal parts when value divisible by parts."
     )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     result = split_integer(7, 1)
     assert result == [7], (
-        "should return part equals to value when split into one part."
+        "Should return part equals to value when split into one part."
     )
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     result = split_integer(18, 5)
     assert result == sorted(result), (
-        "parts should be sorted when they are not equal."
+        "Parts should be sorted when they are not equal."
     )
 
 
