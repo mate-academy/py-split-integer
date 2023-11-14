0ca81fa250e8bc56e157424da3f82b9d103f8c77
Vitalii Taran <vitaliy.taran.1993@gmail.com>
2023-11-14 17:51:22 +0200

    Solution_2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,32 +2,27 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 21
-    number_of_parts = 3
-    assert sum(split_integer(value, number_of_parts)) == value, (
+    assert sum(split_integer(21, 3)) == 21, (
         "Sum of the parts should be equal to value!"
     )
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     result = split_integer(6, 2)
-    assert all(result[i - 1] == result[i] for i in range(1, len(result))), (
+    assert all(split_integer(6, 2)[i - 1] == result[i]
+               for i in range(1, len(split_integer(6, 2)))), (
         "Should split into equal parts when value divisible by parts!"
     )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 11
-    number_of_parts = 1
-    result = split_integer(value, number_of_parts)
-    assert result[0] == value, (
+    assert split_integer(11, 1)[0] == 11, (
         "Should return part equals to value when split into one part!"
     )
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(45, 2)
-    assert result == sorted(result), (
+    assert split_integer(45, 2) == sorted(split_integer(45, 2)), (
         "Parts should be sorted when they are not equal!"
     )
 
