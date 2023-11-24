a7f53974e09021c9bccab6e1539240dec5cd9aff
ve4nick <ve4nick98@gmail.com>
2023-11-24 13:38:19 +0200

    removed unnecessary variables

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,8 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(45, 5)
-    assert min(result) == max(result)
+    assert min(split_integer(45, 5)) == max(split_integer(45, 5))
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -15,8 +14,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(123, 4)
-    assert sorted(result) == result
+    assert sorted(split_integer(123, 4)) == split_integer(123, 4)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
