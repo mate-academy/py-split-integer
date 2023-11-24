429efbe1f2b0c09554faa3a263bfb3ce11a47c7f
Marina <marina.ua13@gmail.com>
2023-11-24 22:11:02 +0200

    fixed all tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,20 +6,20 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(value=6, number_of_parts=2)
+    result = split_integer(6, 2)
     assert all(part == 6 // 2 for part in result)
-    assert max(result) - min(result) >= 1
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(value=17, number_of_parts=1) == [17]
+    result = split_integer(17, 1)
+    assert result == [17]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    sort_part = split_integer(value=17, number_of_parts=4)
-    assert sort_part == [4, 4, 4, 5]
+    result = split_integer(17, 4)
+    assert result == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    zero_part = split_integer(value=8, number_of_parts=12)
-    assert zero_part[0] == 0
+    result = split_integer(8, 12)
+    assert result == [0] * (12 - 8) + [1] * 8
