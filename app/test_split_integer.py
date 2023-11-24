f0769b5254ae077191b32252ea5d44dbd5101183
Marina <marina.ua13@gmail.com>
2023-11-24 22:27:34 +0200

    fixed all tests last version

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -16,8 +16,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-    assert result == sorted(result)
+    assert sorted(split_integer(17, 4))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
