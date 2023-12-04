1e0fb4e677ad06528fba34ac784a8e13c50ab352
Vlad <mr.darmstadtium@gmail.com>
2023-12-04 19:28:36 +0100

    fixed some flake8

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -12,8 +12,8 @@ from app.split_integer import split_integer
         (10, 10)
     ]
 )
-def test_sum_of_the_parts_should_be_equal_to_value(value: int,
-                                                   number_of_parts: int) -> None:
+def test_sum_of_the_parts_should_be_equal_to_value(
+        value: int, number_of_parts: int) -> None:
     assert (
             sum(split_integer(value, number_of_parts)) == value
     ), (f"Function split_integer({value}, {number_of_parts}) "
@@ -29,9 +29,8 @@ def test_sum_of_the_parts_should_be_equal_to_value(value: int,
         (100, 4, [25, 25, 25, 25])
     ]
 )
-def test_should_split_into_equal_parts_when_value_divisible_by_parts(value: int,
-                                                                     number_of_parts: int,
-                                                                     parts: list) -> None:
+def test_should_split_into_equal_parts_when_value_divisible_by_parts(
+        value: int, number_of_parts: int, parts: list) -> None:
     assert (
             split_integer(value, number_of_parts) == parts
     ), (f"Function split_integer({value}, {number_of_parts}) "
@@ -46,8 +45,8 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(value: int,
         (0, 1)
     ]
 )
-def test_should_return_part_equals_to_value_when_split_into_one_part(value: int,
-                                                                     number_of_parts: int) -> None:
+def test_should_return_part_equals_to_value_when_split_into_one_part(
+        value: int, number_of_parts: int) -> None:
     assert (
             split_integer(value, number_of_parts)[0] == value
     ), (f"Function split_integer({value}, {number_of_parts}) "
@@ -62,8 +61,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part(value: int,
         (19, 5)
     ]
 )
-def test_parts_should_be_sorted_when_they_are_not_equal(value: int,
-                                                        number_of_parts: int) -> None:
+def test_parts_should_be_sorted_when_they_are_not_equal(
+        value: int, number_of_parts: int) -> None:
     assert (
             split_integer(value, number_of_parts) == sorted(split_integer(value, number_of_parts))
     ), (f"Function split_integer should return "
@@ -78,9 +77,8 @@ def test_parts_should_be_sorted_when_they_are_not_equal(value: int,
         (1, 2, [0, 1]),
     ]
 )
-def test_should_add_zeros_when_value_is_less_than_number_of_parts(value: int,
-                                                                  number_of_parts: int,
-                                                                  parts: list) -> None:
+def test_should_add_zeros_when_value_is_less_than_number_of_parts(
+        value: int, number_of_parts: int, parts: list) -> None:
     assert (
             split_integer(value, number_of_parts) == parts
     ), (f"Function split_integer should return "
