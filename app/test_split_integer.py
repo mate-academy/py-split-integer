8b995ba915cb537a7343ceb2c68e25b6b61191a7
Vlad <mr.darmstadtium@gmail.com>
2023-12-04 19:15:00 +0100

    wrote all code

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -14,8 +14,8 @@ from app.split_integer import split_integer
 )
 def test_sum_of_the_parts_should_be_equal_to_value(value: int,
                                                    number_of_parts: int) -> None:
-    assert(
-        sum(split_integer(value, number_of_parts)) == value
+    assert (
+            sum(split_integer(value, number_of_parts)) == value
     ), (f"Function split_integer({value}, {number_of_parts}) "
         f"should return parts which sum will be equal to {value}")
 
@@ -33,7 +33,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(value: int,
                                                                      number_of_parts: int,
                                                                      parts: list) -> None:
     assert (
-        split_integer(value, number_of_parts) == parts
+            split_integer(value, number_of_parts) == parts
     ), (f"Function split_integer({value}, {number_of_parts}) "
         f"should return {parts}")
 
@@ -49,13 +49,39 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(value: int,
 def test_should_return_part_equals_to_value_when_split_into_one_part(value: int,
                                                                      number_of_parts: int) -> None:
     assert (
-        split_integer(value, number_of_parts)[0] == value
-    )
+            split_integer(value, number_of_parts)[0] == value
+    ), (f"Function split_integer({value}, {number_of_parts}) "
+        f"should return parts which will be equal to {value}")
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+@pytest.mark.parametrize(
+    "value, number_of_parts",
+    [
+        (3, 2),
+        (345, 4),
+        (19, 5)
+    ]
+)
+def test_parts_should_be_sorted_when_they_are_not_equal(value: int,
+                                                        number_of_parts: int) -> None:
+    assert (
+            split_integer(value, number_of_parts) == sorted(split_integer(value, number_of_parts))
+    ), (f"Function split_integer should return "
+        f"{sorted(split_integer(value, number_of_parts))}, "
+        f"got {split_integer(value, number_of_parts)} instead")
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+@pytest.mark.parametrize(
+    "value, number_of_parts, parts",
+    [
+        (3, 5, [1, 1, 1, 0, 0]),
+        (1, 2, [1, 0]),
+    ]
+)
+def test_should_add_zeros_when_value_is_less_than_number_of_parts(value: int,
+                                                                  number_of_parts: int,
+                                                                  parts: list) -> None:
+    assert (
+            split_integer(value, number_of_parts) == parts
+    ), (f"Function split_integer should return "
+        f"{parts}, got {split_integer(value, number_of_parts)} instead")
