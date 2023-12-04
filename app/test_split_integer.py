bfeaf7c2eb8718a1c2445f5dd0cafbc472fddae1
Vlad <mr.darmstadtium@gmail.com>
2023-12-04 19:25:14 +0100

    pass all tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -74,8 +74,8 @@ def test_parts_should_be_sorted_when_they_are_not_equal(value: int,
 @pytest.mark.parametrize(
     "value, number_of_parts, parts",
     [
-        (3, 5, [1, 1, 1, 0, 0]),
-        (1, 2, [1, 0]),
+        (3, 5, [0, 0, 1, 1, 1]),
+        (1, 2, [0, 1]),
     ]
 )
 def test_should_add_zeros_when_value_is_less_than_number_of_parts(value: int,
