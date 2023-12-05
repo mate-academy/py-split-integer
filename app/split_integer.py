906a2008ecf456862829931f859a2ed54e3230bd
Yuliia Valko <yuliia.valko1@gmail.com>
2023-12-05 15:40:57 +0200

    Solution_1

--- app/split_integer.py
+++ app/split_integer.py
@@ -5,16 +5,3 @@ def split_integer(value: int, number_of_parts: int) -> list:
         parts.append(value // parts_left)
         value -= next_number
     return parts
-
-
-print(split_integer(8, 3))
-
-
-# def split_and_increment_the_last_number(value: int, number_of_parts: int):
-#     result_list = [value // number_of_parts] * number_of_parts
-#     if value % number_of_parts != 0:
-#         result_list[-1] += 1
-#     return result_list
-#
-#
-# print(split_and_increment_the_last_number(8, 3)) #[2, 2, 3]
--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,8 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(8, 3)) == 8, "sum of the parts should be equal to value"
+    assert sum(split_integer(8, 3)) == 8,\
+        "sum of the parts should be equal to value"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -14,13 +15,16 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(8, 1) == [8], "should return part equals to value when split into one part"
+    assert split_integer(8, 1) == [8],\
+        "should return part equals to value when split into one part"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     result = split_integer(8, 3)
-    assert result == sorted(result), "parts should be sorted when they are not equal"
+    assert result == sorted(result),\
+        "parts should be sorted when they are not equal"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(1, 2) == [0, 1], "should add zeros when value is less than number of parts"
+    assert split_integer(1, 2) == [0, 1],\
+        "should add zeros when value is less than number of parts"
