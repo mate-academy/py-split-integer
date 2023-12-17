a906eee74488408b3e2903c4b0967e5c8ec550d0
Volodymyr Kovalenko <380974261314kv@gmail.com>
2023-12-17 10:42:27 +0200

    func test_split_integer fixed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert len(split_integer(12, 3)) == 3
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
