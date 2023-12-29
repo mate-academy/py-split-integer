3b15c43d951020c3f33c5541468780b83a490ae6
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2023-12-29 15:35:40 +0100

    rewritten tests in one line, got rid of "for" loops,  py-split-integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -14,19 +14,22 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
     assert (
-            len(split_integer(value=15, number_of_parts=1)) == 1
-            and split_integer(value=15, number_of_parts=1)[0] == 15
+        len(split_integer(value=15, number_of_parts=1)) == 1
+        and split_integer(value=15, number_of_parts=1)[0] == 15
     )
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
     assert (
-            sorted(split_integer(value=27, number_of_parts=6))
-            == split_integer(value=27, number_of_parts=6)
+        sorted(split_integer(value=27, number_of_parts=6))
+        == split_integer(value=27, number_of_parts=6)
     )
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
 
-    assert split_integer(value=7, number_of_parts=11) == [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+    assert (
+        split_integer(value=7, number_of_parts=11)
+        == [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+    )
