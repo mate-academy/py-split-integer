01065550f53eab850aa38f71dd9f05408e6a5e64
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2023-12-29 15:34:27 +0100

    rewritten tests in one line, got rid of "for" loops,  py-split-integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,49 +2,31 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 17
-    parts = 5
 
-    result = split_integer(value=value, number_of_parts=parts)
-
-    assert sum(result) == value
+    assert sum(split_integer(value=17, number_of_parts=5)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 15
-    parts = 5
-    element = value / parts
 
-    result = split_integer(value=value, number_of_parts=parts)
-
-    for el in result:
-        assert el == element
+    assert split_integer(value=15, number_of_parts=5) == [3, 3, 3, 3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 15
-    parts = 1
 
-    result = split_integer(value=value, number_of_parts=parts)
-
-    assert len(result) == parts and result[0] == value
+    assert (
+            len(split_integer(value=15, number_of_parts=1)) == 1
+            and split_integer(value=15, number_of_parts=1)[0] == 15
+    )
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 27
-    parts = 6
 
-    result = split_integer(value=value, number_of_parts=parts)
-
-    assert sorted(result) == result
+    assert (
+            sorted(split_integer(value=27, number_of_parts=6))
+            == split_integer(value=27, number_of_parts=6)
+    )
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 7
-    parts = 11
-    difference = parts - value
 
-    result = split_integer(value=value, number_of_parts=parts)
-
-    for value in result[:difference]:
-        assert value == 0
+    assert split_integer(value=7, number_of_parts=11) == [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
