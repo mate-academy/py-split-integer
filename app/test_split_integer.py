ff80dd5118a8e8d4d0c5c590e0fe20ad141f5f93
Andrii Zubchenko <dissom1987@gmail.com>
2024-01-07 17:38:43 +0200

    solution2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -11,27 +11,21 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     value = 12
     num_parts = 3
     parts = split_integer(value, num_parts)
-    assert (
-        all(part == value // num_parts for part in parts)
+    assert all(
+        part == value // num_parts for part in parts
     ), "Each part should have the same value"
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     parts = split_integer(2, 1)
-    assert (
-        parts[0] == 2
-    ), f"Part should be equal value if {1} == 1"
+    assert parts[0] == 2, f"Part should be equal value if {1} == 1"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     parts = split_integer(64, 7)
-    assert (
-        parts == sorted(parts)
-    ), "Parts should be sorted"
+    assert parts == sorted(parts), "Parts should be sorted"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     parts = split_integer(6, 10)
-    assert (
-        part == 0 for part in parts
-    )
+    assert (part == 0 for part in parts)
