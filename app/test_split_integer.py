cd5b16e3f7c0681fbf5f1581246637ccaa6e1892
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2024-01-10 00:47:03 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,11 +2,11 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(8, 4)) == 8
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(8, 2) == [4, 4]
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -14,12 +14,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(1, 2) == [0, 1]
-
-f test_difference_between_max_and_min_should_be_less_than_or_equal_to_1() -> None:
-    result = split_integer(17, 4)
-    assert max(result) - min(result) <= 1
+    assert split_integer(2, 3) == [0, 1, 1]
