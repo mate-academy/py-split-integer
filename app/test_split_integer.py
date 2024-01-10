fbb709250417905346a386adaedf739fe2a9db2b
Artur Poltser <polcerartur@gmail.com>
2024-01-10 17:02:01 +0200

    Second Fix

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,33 +2,28 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert (
-        sum(split_integer(100, 3)) == 100
-    ), f"Sum of parts: {sum(split_integer(100, 3))}, should be equal to 100"
+    assert sum(split_integer(100, 3)) == 100, \
+        f"Sum of parts: {sum(split_integer(100, 3))}, should be equal to 100"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     result = split_integer(100, 2)
-    assert (
-        result == [50, 50]
-    ), (
-        f"The difference between the {max(result)} and "
+    assert result == [50, 50], \
+        f"The difference between the {max(result)} and " \
         f"{min(result)} number in the array should be <= 1"
-    )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     result = split_integer(100, 1)
-    assert result == [100], (
+    assert result == [100], \
         f"{result} should be equal to [100] if number of parts is 1"
-    )
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(100, 3) == [33, 33, 34], "Result list should be sorted"
+    assert split_integer(100, 3) == [33, 33, 34], \
+        "Result list should be sorted"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(3, 4) == [0, 1, 1, 1], (
+    assert split_integer(3, 4) == [0, 1, 1, 1], \
         "Function should add zeros when value is less than number of parts"
-    )
