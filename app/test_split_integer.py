19a78f59ea9ae14fbd12154174a998ac5ad82362
Valeriia Danyliuk <bladeryz@gmail.com>
2024-01-15 18:19:47 +0200

    Made test_should_split_into_equal_parts_when_value_divisible_by_parts less complicated

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,8 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    for part in split_integer(8, 4):
-        assert part == 2
+    assert split_integer(8, 4) == [2, 2, 2, 2]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
