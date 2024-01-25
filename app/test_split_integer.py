7bd2f83037b608484045cd92ca013b81919d975a
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-01-25 15:18:43 +0200

    Fix flake8 problems

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,5 +1,3 @@
-import pytest
-
 from app.split_integer import split_integer
 
 
@@ -7,14 +5,20 @@ class TestSplitInteger:
     def test_sum_of_the_parts_should_be_equal_to_value(self) -> None:
         assert sum(split_integer(10, 2)) == 10
 
-    def test_should_split_into_equal_parts_when_value_divisible_by_parts(self) -> None:
+    def test_should_split_into_equal_parts_when_value_divisible_by_parts(
+        self
+    ) -> None:
         assert split_integer(20, 4) == [5, 5, 5, 5]
 
-    def test_should_return_part_equals_to_value_when_split_into_one_part(self) -> None:
+    def test_should_return_part_equals_to_value_when_split_into_one_part(
+        self
+    ) -> None:
         assert split_integer(12, 1) == [12]
 
     def test_parts_should_be_sorted_when_they_are_not_equal(self) -> None:
         assert split_integer(23, 5) == [4, 4, 5, 5, 5]
 
-    def test_should_add_zeros_when_value_is_less_than_number_of_parts(self) -> None:
+    def test_should_add_zeros_when_value_is_less_than_number_of_parts(
+            self
+    ) -> None:
         assert 0 in split_integer(7, 8)
