c17f96b82e0c267bfe5014b127dbcbe169d5513e
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-26 17:34:01 +0200

    Update split_integer.py

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,7 +1,5 @@
 def split_integer(value: int, number_of_parts: int) -> list:
     parts = []
-    if value < number_of_parts:
-        value *= 10
     for parts_left in range(number_of_parts, 0, -1):
         next_number = value // parts_left
         parts.append(value // parts_left)
