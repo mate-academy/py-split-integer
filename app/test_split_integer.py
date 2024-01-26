809cb9ee231379e3a8cb52d0ee8c61cc094c1916
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-26 17:50:58 +0200

    Update test_split_integer.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,20 +1,21 @@
-import pytest
-
 from app.split_integer import split_integer
 
 
-@pytest.mark.parametrize(
-    "value, parts, expected",
-    [
-        pytest.param(6, 2, [3, 3],
-                     id=" if value % parts is zero - parts should be equal"),
-        pytest.param(8, 1, [8],
-                     id="should return list with value when parts equal one"),
-        pytest.param(32, 6, [5, 5, 5, 5, 6, 6],
-                     id="returned list should be sorted in ascending order"),
-        pytest.param(6, 7, [0, 1, 1, 1, 1, 1, 1],
-                     id="should add zeros to value when parts > value")
-    ]
-)
-def test_split_integer(value: int, parts: int, expected: list[int]) -> None:
-    assert split_integer(value, parts) == expected
+def test_sum_of_the_parts_should_be_equal_to_value() -> None:
+    assert split_integer(17, 4) == [4, 4, 4, 5]
+
+
+def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
+    assert split_integer(6, 2) == [3, 3]
+
+
+def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
+    assert split_integer(8, 1) == [8]
+
+
+def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+
+
+def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
+    assert split_integer(6, 7) == [0, 1, 1, 1, 1, 1, 1]
