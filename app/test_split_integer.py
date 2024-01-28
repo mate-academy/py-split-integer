a7b965c8d6144be9ac448c39134d1f88ef09c5c2
Kyrylo Storozhenko <busterakel@gmail.com>
2024-01-28 20:05:00 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert len(set(split_integer(9, 3))) == 1
+    assert split_integer(9, 3) == [3, 3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
