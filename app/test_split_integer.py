faf674a295de75b9e6c4f212bdc59ec5948faaf3
Mykola <mykola22dubyna@gmail.com>
2024-01-28 16:27:53 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(18, 3)[-1] * 3 == 18
+    assert split_integer(18, 3) == [6, 6, 6]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
