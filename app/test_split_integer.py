4b16f0d3e4ce31ed0bf7720a5a4b3ddf56b78106
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2024-01-31 17:31:32 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -20,8 +20,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert (split_integer(93, 6) == [
-        15, 15, 15, 16, 16, 16]), \
-        "Test parts should be sorted when they are not equal"
+        15, 15, 15, 16, 16, 16]), (
+        "Test parts should be sorted when they are not equal")
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
