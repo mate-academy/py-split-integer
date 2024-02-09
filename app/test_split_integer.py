55fe89f3b6823c2cec3e9dd38a26dfa705fb0e72
Ihor Tutak <igor2tak2212@gmail.com>
2024-02-09 15:09:31 +0200

    fixed the function to match the name

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -22,5 +22,5 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    goals = split_integer(9, 0)
-    assert goals == []
+    goals = split_integer(3, 5)
+    assert goals == [0, 0, 1, 1, 1]
