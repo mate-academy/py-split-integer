767ea81ae977929c7aebf0b87249c9969d2d0ce1
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-02-12 23:26:16 +0200

    needed updates

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,28 +6,16 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 6
-    number_of_parts = 2
-    result = split_integer(value, number_of_parts)
-    assert result == [3, 3]
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 8
-    number_of_parts = 1
-    result = split_integer(value, number_of_parts)
-    assert result == [8]
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 32
-    number_of_parts = 6
-    result = split_integer(value, number_of_parts)
-    assert result == [5, 5, 5, 5, 6, 6]
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 2
-    number_of_parts = 5
-    result = split_integer(value, number_of_parts)
-    assert result == [0, 0, 0, 1, 1]
+    assert split_integer(2, 5) == [0, 0, 0, 1, 1]
