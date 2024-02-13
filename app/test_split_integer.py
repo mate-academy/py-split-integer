7b5137c4012f39255fdb512cb592b0a79863e166
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-02-13 20:37:28 +0200

    The zeros test was fixed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -24,4 +24,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     parts = split_integer(3, 8)
     print(parts)
-    assert parts == [0, 0, 0, 0, 0, 0, 0, 0]
+    assert parts == [0, 0, 0, 0, 0, 1, 1, 1]
