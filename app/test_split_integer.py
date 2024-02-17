edc47a2d40f32949481f86bd38860b1bc3facebd
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-02-17 18:04:37 +0200

    'Del_extra_variables'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,13 +2,11 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result = split_integer(6, 2)
-    assert sum(result) == 6
+    assert sum(split_integer(6, 2)) == 6
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(10, 5)
-    assert result == [2, 2, 2, 2, 2]
+    assert split_integer(10, 5) == [2, 2, 2, 2, 2]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -18,10 +16,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(32, 6)
-    assert result == [5, 5, 5, 5, 6, 6]
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(2, 5)
-    assert result == [0, 0, 0, 1, 1]
+    assert split_integer(2, 5) == [0, 0, 0, 1, 1]
