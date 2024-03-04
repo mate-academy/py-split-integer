e9fbacc851645d210b845efa71b694fa81ebe134
Roman Sokolov <gnonasis@gmail.com>
2024-03-04 13:31:03 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert len(split_integer(5, 3)) == 3
+    assert sum(split_integer(5, 3)) == 5
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
