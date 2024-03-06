b30b54d5c747e496ce96ee498df06c6997ad16fb
donbas777 <renary34@gmail.com>
2024-03-06 20:02:44 +0200

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,30 +2,22 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 10
-    number_of_parts = 3
-    return_list = split_integer(value, number_of_parts)
-    assert sum(return_list) == value
+    return_list = split_integer(10, 3)
+    assert sum(return_list) == 10
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 15
-    number_of_parts = 5
-    return_list = split_integer(value, number_of_parts)
+    return_list = split_integer(15, 5)
     assert return_list == [3, 3, 3, 3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 100
-    number_of_parts = 1
-    return_list = split_integer(value, number_of_parts)
-    assert return_list == [value]
+    return_list = split_integer(100, 1)
+    assert return_list == [100]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 17
-    number_of_parts = 4
-    return_list = split_integer(value, number_of_parts)
+    return_list = split_integer(17, 4)
     assert return_list == [4, 4, 4, 5]
 
 
