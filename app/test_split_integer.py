2e54eda83702275d07e7716cfe84071fcd2eaa16
Kyrylo <kbuchinskiy1@gmail.com>
2024-03-08 23:32:22 +0200

    Fix tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,12 +2,12 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(12, 4)) == 10
+    assert sum(split_integer(12, 4)) == 12
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     spl_res = split_integer(12, 4)
-    assert spl_res[0] == spl_res[1] == spl_res[2] == spl_res[3]
+    assert all(part == spl_res[0] for part in spl_res)
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -15,8 +15,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(13, 4) == [4, 4, 5]
+    assert split_integer(13, 4) == [3, 3, 3, 4]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(1, 2) == [1, 0]
+    assert split_integer(1, 2) == [0, 1]
