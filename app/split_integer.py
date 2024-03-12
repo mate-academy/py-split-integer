5cd8d378496bee881d278ef4511b7be2e7dc09d3
Olga <olgahrytsiuk777@gmail.com>
2024-03-12 23:46:10 +0100

    Solution

--- app/split_integer.py
+++ app/split_integer.py
@@ -2,6 +2,8 @@ def split_integer(value: int, number_of_parts: int) -> list:
     parts = []
     for parts_left in range(number_of_parts, 0, -1):
         next_number = value // parts_left
-        parts.append(value // parts_left)
+        parts.append(next_number)
         value -= next_number
+    for _ in range(number_of_parts - len(parts)):
+        parts.append(0)
     return parts
--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,34 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    goal = split_integer(6, 2)
+    assert (
+        sum(goal) == 6
+    )
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    goal = split_integer(6, 2)
+    assert len(goal) == 2
+    assert goal[0] == goal[1]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    goal = split_integer(8, 1)
+    assert goal == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    goal = split_integer(17, 4)
+    assert sorted(goal) == goal
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    goal = split_integer(10, 4)
+    assert len(goal) == 4
+    assert all(element == 0 for element in goal)
+
+
+def test_could_split_on_different_parts() -> None:
+    goal = split_integer(10, 3)
+    assert len(set(goal)) != 1
