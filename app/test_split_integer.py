0f227de8906ce9d6f25c934740a683dad297f8e6
Iryna Lyakhova <ippk0629@gmail.com>
2024-03-12 11:11:54 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,36 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    value = 10
+    number_of_parts = 3
+    parts = split_integer(value, number_of_parts)
+    assert sum(parts) == value
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    value = 12
+    number_of_parts = 4
+    parts = split_integer(value, number_of_parts)
+    assert all(p == value // number_of_parts for p in parts)
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    value = 8
+    number_of_parts = 1
+    parts = split_integer(value, number_of_parts)
+    assert parts == [value]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    value = 17
+    number_of_parts = 4
+    parts = split_integer(value, number_of_parts)
+    assert parts == sorted(parts)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    value = 4
+    number_of_parts = 6
+    parts = split_integer(value, number_of_parts)
+    expected = [0, 0, 1, 1, 1, 1]
+    assert parts == expected
