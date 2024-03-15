edb02ddd6bb89d4abff14f81fadc57bdec596b79
skeletronys <mykytcey@gmail.com>
2024-03-15 01:04:49 +0100

    new test

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert 17 == sum(split_integer(17, 4))
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -14,8 +14,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    sorted_list = sorted(split_integer(17, 4))
-    assert sorted_list == [4, 4, 4, 5]
+    assert sorted(split_integer(17, 4)) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
