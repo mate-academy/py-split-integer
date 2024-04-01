234516c8f22fedf75e94aba348c2c36111650d61
kekW <panicua12@gmail.com>
2024-04-01 14:25:26 +0300

    removed messages from asserts, minor fixes with formating

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,28 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert (sum(split_integer(6, 2)) == 6
-            ), "sum of the result list should be equal to start value"
+    assert sum(split_integer(6, 2)) == 6
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert (split_integer(110, 10)
-            == [11, 11, 11, 11, 11, 11, 11, 11, 11, 11]
-            ), ("if start value divisible by number_of_parts, "
-                "all parts should be equal")
+    assert split_integer(110, 10) == [11] * 10
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert (split_integer(13, 1) == [13]
-            ), "should return list of start value if number_of_parts == 1"
+    assert split_integer(13, 1) == [13]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert (split_integer(17, 4) == [4, 4, 4, 5]
-            ), "return should be a sorted list if parts are not equal"
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert (split_integer(2, 5) == [0, 0, 0, 1, 1]
-            ), ("return should have number of zeros inside equal to "
-                "'number_of_parts - value' if value < number_of_parts")
+    assert split_integer(2, 5) == [0, 0, 0, 1, 1]
