2dbcd4947c19012ef426f905e846e61b1d351aa1
Marharyta Marchenko <ritanika12@gmail.com>
2024-04-01 21:23:05 +0200

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -20,4 +20,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert 5 == sum(split_integer(5, 8))
+    assert len(split_integer(5, 8)) == 8
