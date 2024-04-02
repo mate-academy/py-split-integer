a4617bca2972b0595d2cc51b16e3b64051ea142b
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-04-02 18:34:33 +0300

    Is it possible with an error in flake8?

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -19,36 +19,36 @@ class TestSplit:
             second_int: int,
             result: list
     ) -> None:
-        assert split_integer(first_int, second_int) == \
-               result, f"Sum of the parts should be equal to value {first_int}"
+        assert split_integer(first_int, second_int) == (
+            result, f"Sum of the parts should be equal to value {first_int}")
 
     def test_should_split_into_equal_parts_when_value_divisible_by_parts(
-            self
+            self,
+            first_int: int,
+            second_int: int
     ) -> None:
-        first_int = 6
-        second_int = 2
-        assert split_integer(first_int, second_int) == [3, 3],\
-            "Test should split into equal parts when value is divisible by"
+        assert (split_integer(first_int, second_int) == [3, 3]
+                ), "Test should split equal parts when value is divisible by"
 
     def test_should_return_part_equals_to_value_when_split_into_one_part(
             self
     ) -> None:
         first_int = 8
         second_int = 1
-        assert split_integer(first_int, second_int) == [8],\
-            "Test should return part equals to value when split into one part"
+        assert (split_integer(first_int, second_int) == [8]
+                ), "Test should return part equals to value"
 
     def test_parts_should_be_sorted_when_they_are_not_equal(self) -> None:
         first_int = 17
         second_int = 4
         result = [4, 4, 4, 5]
-        assert split_integer(first_int, second_int) == sorted(result),\
-            "Test parts should be sorted when they are not equal"
+        assert (split_integer(first_int, second_int) == sorted(result)
+                ), "Test parts should be sorted when they are not equal"
 
     def test_should_add_zeros_when_value_is_less_than_number_of_parts(
             self
     ) -> None:
         first_int = 3
         second_int = 5
-        assert split_integer(first_int, second_int) == [0, 0, 1, 1, 1],\
-            "Test should add zeros when value is less than number of parts"
+        assert (split_integer(first_int, second_int) == [0, 0, 1, 1, 1]
+                ), "Test should add zeros"
