d45cf970d765a75cef88250cef89bd203593e746
bwaah1 <chykivan@gmail.com>
2024-04-03 12:25:56 +0300

    Fix flake8 error

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -4,7 +4,8 @@ from app.split_integer import split_integer
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     initial_value = 32
     number_of_parts = 6
-    assert sum(split_integer(initial_value, number_of_parts)) == initial_value
+    assert (sum(split_integer(initial_value, number_of_parts))
+            == initial_value)
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -22,8 +23,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     initial_value = 16
     number_of_parts = 4
-    assert (split_integer(initial_value, number_of_parts) ==
-            sorted(split_integer(initial_value, number_of_parts)))
+    assert (split_integer(initial_value, number_of_parts)
+            == sorted(split_integer(initial_value, number_of_parts)))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
