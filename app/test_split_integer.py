a0b424826c9dd9bea4cc25d822ec50f4f1494bcf
Kateryna Shevchenko <sidelnikks2@gmail.com>
2024-04-03 13:45:34 +0300

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,53 @@
 from app.split_integer import split_integer
+import pytest
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+@pytest.mark.parametrize("value, number_of_parts, expected_result", [
+    (8, 1, [8]),
+    (6, 2, [3, 3]),
+    (17, 4, [4, 4, 4, 5]),
+    (32, 6, [5, 5, 5, 5, 6, 6])
+])
+def test_sum_of_the_parts_should_be_equal_to_value(value, number_of_parts, expected_result) -> None:
+    assert value == sum(expected_result)
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+@pytest.mark.parametrize("value, number_of_parts, expected_result", [
+    (8, 1, [8]),
+    (6, 2, [3, 3]),
+    (17, 4, [4, 4, 4, 5]),
+    (32, 6, [5, 5, 5, 5, 6, 6])
+])
+def test_should_split_into_equal_parts_when_value_divisible_by_parts(value, number_of_parts, expected_result) -> None:
+    assert len(split_integer(value, number_of_parts)) == number_of_parts
+    assert max(expected_result) - min(expected_result) <= 1
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+@pytest.mark.parametrize("value, number_of_parts, expected_result", [
+    (8, 1, [8]),
+    (6, 2, [3, 3]),
+    (17, 4, [4, 4, 4, 5]),
+    (32, 6, [5, 5, 5, 5, 6, 6])
+])
+def test_should_return_part_equals_to_value_when_split_into_one_part(value, number_of_parts, expected_result) -> None:
+    assert (split_integer(value, number_of_parts)) == expected_result
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+@pytest.mark.parametrize("value, number_of_parts", [
+    (8, 1),
+    (6, 2),
+    (17, 4),
+    (32, 6)
+])
+def test_parts_should_be_sorted_when_they_are_not_equal(value, number_of_parts) -> None:
+    assert split_integer(value, number_of_parts) == sorted(split_integer(value, number_of_parts))
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+@pytest.mark.parametrize("value, number_of_parts", [
+    (8, 1),
+    (6, 2),
+    (17, 4),
+    (32, 6)
+])
+def test_should_add_zeros_when_value_is_less_than_number_of_parts(value, number_of_parts) -> None:
+    assert not value < number_of_parts
