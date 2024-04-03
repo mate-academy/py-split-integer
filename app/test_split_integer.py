b6a3496a4a01640509a5a5dd62886b04434cd83b
Mikhail Yemets <mikhailyemets.pl@gmail.com>
2024-04-03 21:05:09 +0200

    implement round braces for error message

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -3,25 +3,30 @@ from app.split_integer import split_integer
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     parts = split_integer(32, 6)
-    assert sum(parts) == 32, \
+    assert sum(parts) == 32, (
         f"The sum of parts {parts}"f" does not equal {sum(parts)}"
+    )
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(6, 2) == [3, 3], \
+    assert split_integer(6, 2) == [3, 3], (
         "The value was not split into equal parts when divisible."
+    )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(8, 1) == [8], \
+    assert split_integer(8, 1) == [8], (
         "The value was not returned as a single part when split into one part."
+    )
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(17, 4) == [4, 4, 4, 5], \
+    assert split_integer(17, 4) == [4, 4, 4, 5], (
         "The parts are not sorted properly when they are not equal."
+    )
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(2, 4) == [0, 0, 1, 1], \
+    assert split_integer(2, 4) == [0, 0, 1, 1], (
         "Incorrectly added 0's the value is less than the number of parts."
+    )
