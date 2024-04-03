1f23eae6d0c1a1b532b6f25d007f5fe4eb2c634f
bwaah1 <chykivan@gmail.com>
2024-04-03 21:18:49 +0300

    Simplify code and add comments to asserts

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,33 +2,23 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    initial_value = 32
-    number_of_parts = 6
-    assert (sum(split_integer(initial_value, number_of_parts))
-            == initial_value)
+    assert (sum(split_integer(32, 6))
+            == 32), "Sum of the parts should be equal to value"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    initial_value = 16
-    number_of_parts = 4
-    assert len(set(split_integer(initial_value, number_of_parts))) == 1
+    assert len(set(split_integer(16, 4))) == 1, "When value divisible by parts split into equal parts"
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    initial_value = 8
-    number_of_parts = 1
-    assert split_integer(initial_value, number_of_parts)[0] == initial_value
+    assert split_integer(8, 1)[0] == 8, "Return part equals to value when split into one part"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    initial_value = 16
-    number_of_parts = 4
-    assert (split_integer(initial_value, number_of_parts)
-            == sorted(split_integer(initial_value, number_of_parts)))
+    assert (split_integer(16, 4)
+            == sorted(split_integer(16, 4))), "Parts should be sorted when they are not equal"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    initial_value = 6
-    number_of_parts = 10
-    diff = number_of_parts - initial_value
-    assert split_integer(initial_value, number_of_parts)[:diff] == [0] * diff
+    diff = 10 - 6
+    assert split_integer(6, 10)[:diff] == [0] * diff, "Should add zeros when value is less than number of parts"
