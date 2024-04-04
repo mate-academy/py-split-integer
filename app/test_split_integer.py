776006beb1abfe86ba12ff7e3884f8ef0840bf57
NioviIki <charmniovi@tutanota.com>
2024-04-04 16:39:55 +0300

    solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -4,29 +4,43 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(100, 3)) == 100
+    assert (
+        sum(split_integer(100, 3)) == 100
+    ), "Sum of parts is not equal to the original value"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(100, 4).count(25) == 4
+    assert (
+        split_integer(100, 4).count(25) == 4
+    ), "Value not split into equal parts"
 
 
 @pytest.mark.parametrize(
-    "numbers,expected_result",
+    "value,number_of_parts,expected_result",
     [
-        pytest.param(split_integer(10, 1), [10]),
-        pytest.param(split_integer(10, 10), [1] * 10)
+        (10, 1, [10]),
+        (10, 10, [1] * 10)
+    ],
+    ids=[
+        "Split into one part with value 10",
+        "Split into 10 equal parts with value 1"
     ]
 )
 def test_should_return_part_equals_to_value_when_split_into_one_part(
-    numbers: list, expected_result: int
+    value: int, number_of_parts: int, expected_result: list
 ) -> None:
-    assert numbers == expected_result
+    assert (
+        split_integer(value, number_of_parts) == expected_result
+    ), "Value not split into one part or into 10 equal parts"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(100, 3) == [33, 33, 34]
+    assert (
+        split_integer(100, 3) == [33, 33, 34]
+    ), "Parts are not sorted"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(3, 5) == [0, 0, 1, 1, 1]
+    assert (
+        split_integer(3, 5) == [0, 0, 1, 1, 1]
+    ), "Zeros not added when value is less than the number of parts"
