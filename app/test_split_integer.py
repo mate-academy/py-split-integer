0fa125936f3c27ccdc51f78570611fbe382f1193
Vitalii <vitalikbashkiser@gmail.com>
2024-04-09 00:37:15 +0300

    Solution#2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,19 +1,31 @@
-import pytest
+import unittest
 from app.split_integer import split_integer
 
 
-@pytest.mark.parametrize("value, number_of_parts, expected_result", [
-    (8, 1, [8]),
-    (6, 2, [3, 3]),
-    (17, 4, [4, 4, 4, 5]),
-    (32, 6, [5, 5, 5, 5, 6, 6])
-])
-def test_split_integer(
-        value: int,
-        number_of_parts: int,
-        expected_result: int
-) -> None:
-    """
-    Test for split_integer function.
-    """
-    assert split_integer(value, number_of_parts) == expected_result
+class TestSplitInteger(unittest.TestCase):
+
+    def test_split_integer(self) -> None:
+        """
+        Test for split_integer function.
+        """
+        test_cases = [
+            (8, 1, [8]),
+            (6, 2, [3, 3]),
+            (17, 4, [4, 4, 4, 5]),
+            (32, 6, [5, 5, 5, 5, 6, 6])
+        ]
+
+        for value, number_of_parts, expected_result in test_cases:
+            with self.subTest(
+                    value=value,
+                    number_of_parts=number_of_parts,
+                    expected_result=expected_result
+            ):
+                self.assertEqual(split_integer(
+                    value, number_of_parts),
+                    expected_result
+                )
+
+
+if __name__ == "__main__":
+    unittest.main()
