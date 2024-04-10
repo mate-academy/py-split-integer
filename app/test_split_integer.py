bdcd27c4482efa9368cc82de0fd6f84436e823ec
Vitalii <vitalikbashkiser@gmail.com>
2024-04-11 02:36:09 +0300

    Solution#4

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -4,50 +4,24 @@ from app.split_integer import split_integer
 
 class TestSplit(unittest.TestCase):
 
-    def test_split_integer_zero_parts(self) -> None:
-        """
-        Test for split_integer function with zero parts.
-        """
-        self.assertEqual(
-            split_integer(10, 0), [],
-            "Value must be divided into an empty list"
-            " when the number of parts is zero."
-        )
+    def test_split_integer_single_value_single_part(self) -> None:
+        assert split_integer(8, 1) == [8],\
+            "Value must be divided into a list with 1 element equal to value."
 
-    def test_split_integer_negative_value(self) -> None:
-        """
-        Test for split_integer function with a negative value.
-        """
-        self.assertEqual(
-            split_integer(-10, 3), [-4, -3, -3],
-            "Value must be divided into parts of approximately equal size,"
-            " even with negative values."
-        )
+    def test_split_integer_even_value_two_parts(self) -> None:
+        assert split_integer(6, 2) == [3, 3],\
+            "Even value must be divided into two parts of equal size."
 
-    def test_split_integer_large_value(self) -> None:
-        """
-        Test for split_integer function with a large value.
-        """
-        self.assertEqual(
-            split_integer(1000, 5), [200, 200, 200, 200, 200],
-            "Large value must be divided into equal parts."
-        )
+    def test_split_integer_odd_value_four_parts(self) -> None:
+        assert split_integer(17, 4) == [4, 4, 4, 5],\
+            "Odd value must be divided into four parts" \
+            " with a difference of 1."
 
-    def test_split_integer_single_part(self) -> None:
-        """
-        Test for split_integer function with a single part.
-        """
-        self.assertEqual(
-            split_integer(10, 1), [10],
-            "Value must be divided into a list"
-            " with a single element equal to the original value."
-        )
+    def test_split_integer_large_value_six_parts(self) -> None:
+        assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6],\
+            "Large value must be divided into six parts" \
+            " with a difference of 1."
 
-    def test_split_integer_decimal_value(self) -> None:
-        """
-        Test for split_integer function with a decimal value.
-        """
-        self.assertEqual(
-            split_integer(7, 3), [2, 2, 3],
-            "Value must be divided into parts of approximately equal size."
-        )
+    def test_split_integer_value_largest_part(self) -> None:
+        assert split_integer(5, 3) == [1, 2, 2],\
+            "Value must be divided into three parts with a difference of 1."
