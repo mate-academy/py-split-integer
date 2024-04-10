5e60258c2285179bcb008d371a479cd29c702f86
Sofiia <sonagm9@gmail.com>
2024-04-10 14:32:11 +0300

    'Solution2'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -4,44 +4,31 @@ from app.split_integer import split_integer
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     value = 17
     number_of_parts = 4
-    parts = split_integer(value, number_of_parts)
-    assert sum(parts) == value
-    assert all(parts[i] <= parts[i + 1] for i in range(len(parts) - 1))
+    assert sum(split_integer(value, number_of_parts)) == value
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 32
+    value = 36
     number_of_parts = 6
-    parts = split_integer(value, number_of_parts)
-    assert len(parts) == number_of_parts
-    assert max(parts) - min(parts) <= 1
-    assert sorted(parts) == parts
-    assert all(parts[i] <= parts[i + 1] for i in range(len(parts) - 1))
+    assert all(part == value // number_of_parts for part in split_integer(value, number_of_parts))
+
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     value = 8
     number_of_parts = 1
-    parts = split_integer(value, number_of_parts)
-    assert parts == [value]
-    assert all(parts[i] <= parts[i + 1] for i in range(len(parts) - 1))
+    assert split_integer(value, number_of_parts) == [value]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     value = 17
     number_of_parts = 4
-    parts = split_integer(value, number_of_parts)
-    assert sorted(parts) == parts
-    assert all(parts[i] <= parts[i + 1] for i in range(len(parts) - 1))
+    assert (split_integer(value, number_of_parts)
+            == sorted(split_integer(value, number_of_parts)))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     value = 6
     number_of_parts = 8
-    parts = split_integer(value, number_of_parts)
-    assert sum(parts) == value
-    assert len(parts) == number_of_parts
-    assert max(parts) - min(parts) <= 1
-    assert sorted(parts) == parts
-    assert parts.count(0) == number_of_parts - value
-    assert all(parts[i] <= parts[i + 1] for i in range(len(parts) - 1))
+    assert (split_integer(value, number_of_parts).count(0)
+            == number_of_parts - value)
