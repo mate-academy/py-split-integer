728f918304382d2e499dbcfeaa147cb822a0e3ff
Artem Andriushchenko <link.by.artem@gmail.com>
2024-04-12 03:10:15 +0300

    added assert statement to each test

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,6 +6,9 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert split_integer(6, 2) == [3, 3]
     assert split_integer(17, 4) == [4, 4, 4, 5]
     assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    assert (test_sum_of_the_parts_should_be_equal_to_value.
+            __name__
+            )
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -13,18 +16,30 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert split_integer(6, 2) == [3, 3]
     assert split_integer(17, 4) == [4, 4, 4, 5]
     assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    assert (test_should_split_into_equal_parts_when_value_divisible_by_parts.
+            __name__
+            )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 8) == [1, 1, 1, 1, 1, 1, 1, 1]
     assert split_integer(10, 10) == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    assert (test_should_return_part_equals_to_value_when_split_into_one_part.
+            __name__
+            )
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert split_integer(10, 5) == [2, 2, 2, 2, 2]
     assert split_integer(15, 4) == [3, 4, 4, 4]
+    assert (test_parts_should_be_sorted_when_they_are_not_equal.
+            __name__
+            )
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(3, 5) == [0, 0, 1, 1, 1]
     assert split_integer(1, 3) == [0, 0, 1]
+    assert (test_should_add_zeros_when_value_is_less_than_number_of_parts.
+            __name__
+            )
