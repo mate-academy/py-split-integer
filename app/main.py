3c3e3d9add7017a1207c8ec8e415b5af759d46b1
dvkim28 <d.villarionovich@gmail.com>
2024-04-16 17:03:36 +0200

    Solution

--- app/main.py
+++
@@ -1,9 +0,0 @@
-def shortest_word(sentence: str) -> str:
-    arr = sentence.split()
-    result = arr[0]
-    for i in arr:
-        if len(i) < len(result):
-            result = i
-    return len(result)
-
-print(shortest_word("Call 911"))
