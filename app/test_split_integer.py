5c4c38cb4fdff2e55dfe8d110bbe3d08affe8ec6
Liudmyla <4888814@gmail.com>
2024-04-23 12:57:56 +0300

    'check'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -14,17 +14,6 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert sum(split_integer(32, 6)) == 32
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 6
-    number_of_parts = 2
-    if value / number_of_parts == value // number_of_parts:
-        assert len(set(split_integer(value, number_of_parts))) == 1
-
-
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(8, 1) == [8]
-
-
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     sorted_list = split_integer(17, 4)
     sorted_list.sort()
