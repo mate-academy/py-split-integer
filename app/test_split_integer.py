1f008d7f09f56646e98946cb15a3348954e0b484
Mikola Rozhansky <bymikola@gmail.com>
2024-05-07 21:08:06 +0300

    1

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -10,7 +10,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(36,1) == 36
+    assert split_integer(36,1) == [36]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -18,4 +18,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(2, 3) == [1, 1, 0]
+    assert split_integer(2, 3) == [0, 1, 1]
