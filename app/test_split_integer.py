fd2dabdf34c0dca815e74037f2d000e8b30168ac
VitaliiDats1 <vitalii.8dats@gmail.com>
2024-06-07 17:46:25 +0300

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,16 +6,16 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(20, 4) == [5, 5, 5, 5]
+    assert split_integer(20, 4) == [5, 5, 5, 5], "Verify correctness splitting"
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(10, 1) == [10]
+    assert split_integer(10, 1) == [10], "Splitting number into 1 should return this number"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(23, 5) == [4, 4, 5, 5, 5]
+    assert split_integer(23, 5) == [4, 4, 5, 5, 5], "Verify not equal splitting"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(1, 2) == [0, 1]
+    assert split_integer(1, 2) == [0, 1], "Zero should be added if value is less than part"
