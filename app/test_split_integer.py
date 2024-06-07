cb9b73fd57fc4b2fda96692ac33dc8e56630936f
Sergii Semenets <serhey.semenets@gmail.com>
2024-06-07 15:18:20 +0300

    Solution V.2 whis Error

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,5 +1,5 @@
 from app.split_integer import split_integer
-
+import  pytest
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(49, 7)) == 49
@@ -19,3 +19,17 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(5, 7) == [0, 0, 1, 1, 1, 1, 1]
+
+
+def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
+    assert split_integer(5, 7) == [0, 0, 1, 1, 1, 1, 1]
+
+
+def test_should_return_error_when_value_is_None() -> None:
+    with pytest.raises(TypeError):
+        split_integer(None, 6)
+
+def test_should_return_error_when_number_of_parts_is_None() -> None:
+    with pytest.raises(TypeError):
+        split_integer(5, None)
+
