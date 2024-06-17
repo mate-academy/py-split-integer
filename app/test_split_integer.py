29ceac2a062f88f5b7ac346b0d94a0246496809e
Tymur Shmorhun <shmorguntimur@gmail.com>
2024-06-17 22:14:46 +0300

    solution3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -26,6 +26,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert (
-        0 in split_integer(10, 11)
-    )
+    assert split_integer(10, 11) == [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
