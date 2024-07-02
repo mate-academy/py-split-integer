1741c01e08370b156ffebdf2d30f24dce86283cd
Vova <v.kampanchenko@gmail.com>
2024-07-02 16:08:18 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -19,37 +19,14 @@ def test_sum_of_the_parts_should_be_equal_to_value(
             f"should be equal to {value}")
 
 
-@pytest.mark.parametrize(
-    "value, number_of_parts, result",
-    [
-        (6, 2, [3, 3]),
-        (17, 4, [4, 4, 4, 5]),
-        (32, 6, [5, 5, 5, 5, 6, 6]),
-    ]
-)
-def test_should_split_into_equal_parts_when_value_divisible_by_parts(
-    value: int, number_of_parts: int, result: list
-) -> None:
-    expected = [value // number_of_parts] * number_of_parts
-    assert (split_integer(value, number_of_parts) == expected), \
-        f"{split_integer(value, number_of_parts)} should be equal parts"
+def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
+    assert (split_integer(6, 2) == [3, 3]), \
+        f"{split_integer(6, 2)} should be equal parts"
 
 
-@pytest.mark.parametrize(
-    "value, number_of_parts, result",
-    [
-        (8, 1, [8]),
-        (6, 2, [3, 3]),
-        (17, 4, [4, 4, 4, 5]),
-        (32, 6, [5, 5, 5, 5, 6, 6]),
-    ]
-)
-def test_should_return_part_equals_to_value_when_split_into_one_part(
-    value: int, number_of_parts: int, result: list
-) -> None:
-    expected = [value // number_of_parts]
-    assert (split_integer(value, number_of_parts) == expected), \
-        f"length of {split_integer(value, number_of_parts)} should 1"
+def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
+    assert (split_integer(8, 1) == [8]), \
+        f"length of {split_integer(8, 1)} should 1"
 
 
 @pytest.mark.parametrize(
