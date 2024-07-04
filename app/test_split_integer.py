05a274a22932fe9c650edd72e9745662999df189
hhorhe <horhehorda@gmail.com>
2024-07-04 20:33:35 +0200

    'Solution_2'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -5,32 +5,35 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     value = 17
     number_of_parts = 4
     result = split_integer(value, number_of_parts)
-    assert sum(result) == value, f"Expected sum to be {value}, but got {sum(result)}"
+    assert sum(result) == value
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 12
+    value = 9
     number_of_parts = 3
     result = split_integer(value, number_of_parts)
-    assert result == [4, 4, 4], f"Expected [4, 4, 4], but got {result}"
+    assert all(
+        x == result[0] for x in result
+    )
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     value = 8
     number_of_parts = 1
     result = split_integer(value, number_of_parts)
-    assert result == [8], f"Expected [8], but got {result}"
+    assert result == [value]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 17
-    number_of_parts = 4
+    value = 23
+    number_of_parts = 5
     result = split_integer(value, number_of_parts)
-    assert result == sorted(result), f"Expected sorted parts, but got {result}"
+    assert result == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     value = 3
     number_of_parts = 5
     result = split_integer(value, number_of_parts)
-    assert result == [0, 0, 1, 1, 1], f"Expected [0, 0, 1, 1, 1], but got {result}"
+    expected_result = [0, 0, 1, 1, 1]
+    assert result == expected_result
