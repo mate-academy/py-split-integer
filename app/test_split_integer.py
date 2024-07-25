d8bdfb1e45ae30ce86691e926751a18f36c74a7d
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-07-25 20:00:00 +0300

    sol3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,11 +2,11 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(10, 3)) == 10
+    assert sum(split_integer(12, 4)) == 12
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert max(split_integer(10, 5)) == 2
+    assert split_integer(18, 6) == [3, 3, 3, 3, 3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -14,8 +14,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(19, 17) == sorted(split_integer(19, 17))
+    assert split_integer(20, 3) == sorted(split_integer(20, 3))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert min(split_integer(20, 50)) == 0
+    assert split_integer(3, 6) == [0, 0, 0, 1, 1, 1]
