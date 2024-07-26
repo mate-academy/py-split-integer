a56464b739e8a1d6a29eb64314896dca2910b2e2
Olga-Gynguliak <olga.gynguliak@gmail.com>
2024-07-26 14:53:46 +0300

    "Solution py-split-integer_1"

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -5,13 +5,11 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(8, 1)) == 8
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts(
-        ) -> None:
+def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert split_integer(6, 2) == [3, 3]
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part(
-        ) -> None:
+def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 1) == [8]
 
 
@@ -20,6 +18,5 @@ def test_parts_should_be_sorted_when_they_are_not_equal(
     assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts(
-        ) -> None:
+def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(17, 4) == [4, 4, 4, 5]
