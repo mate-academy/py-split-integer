031f99697ef8d4bc977e325fbce0484128270ad4
Maksim <golevmaksim52@gmail.com>
2024-08-02 23:10:36 +0300

    fix code stile

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,36 +2,25 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 17
-    number_of_parts = 4
-    result = split_integer(value, number_of_parts)
-    assert sum(result) == value
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 12
-    number_of_parts = 4
-    result = split_integer(value, number_of_parts)
-    assert all(x == result[0] for x in result)
+    assert all(x == split_integer(12, 4)[0] for x in split_integer(12, 4))
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 5
-    number_of_parts = 1
-    result = split_integer(value, number_of_parts)
-    assert result == [value]
+    assert split_integer(5, 1) == [5]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 17
-    number_of_parts = 4
-    result = split_integer(value, number_of_parts)
-    assert result == sorted(result)
+    assert split_integer(17, 4) == sorted(split_integer(17, 4))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 3
-    number_of_parts = 5
-    result = split_integer(value, number_of_parts)
-    assert len(result) == number_of_parts
-    assert result.count(0) == number_of_parts - value
+    """I'm using a variable here because
+    I think it's more convenient and the code is cleaner.
+    """
+    result = split_integer(3, 5)
+    assert len(result) == 5
+    assert result.count(0) == 5 - 3
