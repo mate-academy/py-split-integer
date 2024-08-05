b5369da7f730410ea03de1adf304200262872cf4
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-08-05 22:13:08 +0300

    Solution 1.2

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,6 +1,4 @@
-def split_integer(value: int, number_of_parts: int) -> list | int:
-    if not value > number_of_parts:
-        return 0
+def split_integer(value: int, number_of_parts: int) -> list:
     parts = []
     for parts_left in range(number_of_parts, 0, -1):
         next_number = value // parts_left
--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -20,4 +20,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(2, 3) == 0
+    assert split_integer(2, 3) == [0, 1, 1]
