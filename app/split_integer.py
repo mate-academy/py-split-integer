dce0b5eda2911e734d7f4a74e173d0b0c7a673c2
Denys Piuro <denys.pyuro@gmail.com>
2024-08-07 19:54:54 +0300

    Solution

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,7 +1,5 @@
 def split_integer(value: int, number_of_parts: int) -> list:
-    parts = []
-    for parts_left in range(number_of_parts, 0, -1):
-        next_number = value // parts_left
-        parts.append(value // parts_left)
-        value -= next_number
-    return parts
+    parts = [value // number_of_parts] * number_of_parts
+    for i in range(value % number_of_parts):
+        parts[i] += 1
+    return sorted(parts)
