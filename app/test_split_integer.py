6ae269a4d350221465a3f5ffe1e763867616ad42
DSahalatyi <dsahalatyi@gmail.com>
2024-08-08 11:29:53 +0300

    add error messages

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -5,7 +5,8 @@ import pytest
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(17, 4)) == 17
+    assert sum(split_integer(17, 4)) == 17, \
+        "Sum of parts is not equal to value"
 
 
 @pytest.mark.parametrize(
@@ -24,16 +25,20 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(
         parts_number: int,
         result: list
 ) -> None:
-    assert split_integer(value, parts_number) == result
+    assert split_integer(value, parts_number) == result, \
+        f"Parts are not equal, despite {value} is divisible by {parts_number}"
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(8, 1) == [8]
+    assert split_integer(8, 1) == [8], \
+        "Part is not equal to value when split into one part"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(17, 4) == sorted(split_integer(17, 4))
+    assert split_integer(17, 4) == sorted(split_integer(17, 4)), \
+        "Parts are not sorted"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(2, 4) == [0, 0, 1, 1]
+    assert split_integer(2, 4) == [0, 0, 1, 1], \
+        "Zeroes are not added when value < number of parts"
