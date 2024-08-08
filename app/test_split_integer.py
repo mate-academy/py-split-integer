4b819320e6e36fe97ed8e9f7d0dfff876c62caa7
DSahalatyi <dsahalatyi@gmail.com>
2024-08-08 10:08:47 +0300

    Revert "refactor cases"
    This reverts commit fa15553bcec4bb11ad19953050f47a597f462209.

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,29 +1,30 @@
 from app.split_integer import split_integer
 
 
-def test_sum_of_parts_equals_value() -> None:
+def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(17, 4)) == 17
 
 
-def test_parts_are_equal_if_value_divisible_by_number() -> None:
+def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     result = split_integer(8, 2)
     assert result[0] == result[1]
 
+    result = split_integer(9, 2)
+    assert result[0] != result[1]
 
-def test_returns_value_if_number_is_one() -> None:
+
+def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 1) == [8]
 
 
-def test_parts_are_sorted_when_they_are_not_equal() -> None:
+def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     result = split_integer(17, 4)
     assert result == sorted(result)
 
 
-def test_add_zeros_when_value_is_less_than_number() -> None:
+def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     result = split_integer(8, 12)
     assert result[0:4] == [0, 0, 0, 0]
 
-
-def test_add_zeroes_if_value_is_negative_and_less_than_number() -> None:
     result = split_integer(-8, 12)
     assert result[-4:] == [0, 0, 0, 0]
