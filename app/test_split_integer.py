c3869cf5d25b5eab1845afa3409a12c977f1cd3a
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-08-08 11:50:13 +0300

    'added_error_messages'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,30 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(32, 6)) == 32
+    assert (
+        sum(split_integer(32, 6)) == 32
+    ), "5 + 5 + 5 + 5 + 6 + 6 should be equal to 32"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(25, 5) == [5, 5, 5, 5, 5]
+    assert (
+        split_integer(25, 5) == [5, 5, 5, 5, 5]
+    ), "25 is divisible by 5 so it should return [5, 5, 5, 5, 5]"
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(10, 1) == [10]
+    assert (
+        split_integer(10, 1) == [10]
+    ), "When 'numbers_of_parts' = 1, func should return list with one element equal to 'value'"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(18, 4) == sorted((split_integer(18, 4)))
+    assert (
+        split_integer(18, 4) == sorted((split_integer(18, 4)))
+    ), "You should sort your resulting list"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(2, 4) == [0, 0, 1, 1]
+    assert (
+        split_integer(2, 4) == [0, 0, 1, 1]
+    ), "2 < 4, so you need to add zeros to match number of parts"
