f99048babd9b86ab4e17ea1763b6d1c8f720a176
DSahalatyi <dsahalatyi@gmail.com>
2024-08-08 10:08:48 +0300

    Revert "Solution"
    This reverts commit 16734b1c35b9796a6631961d7a630b883d3858aa.

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,29 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(17, 4)) == 17
+    pass
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(8, 2)
-    assert result[0] == result[1]
-
-    result = split_integer(9, 2)
-    assert result[0] != result[1]
+    pass
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(8, 1) == [8]
+    pass
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-    assert result == sorted(result)
+    pass
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(8, 12)
-    assert result[0:4] == [0, 0, 0, 0]
-
-    result = split_integer(-8, 12)
-    assert result[-4:] == [0, 0, 0, 0]
+    pass
