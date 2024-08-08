e22760dbc75d50071889be717d3fdade648e9b42
Vlada <vlada.belayy@mail.com>
2024-08-08 13:33:17 +0300

    added message and writed error in 1 line

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,28 +2,36 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(8, 1)) == 8
+    assert sum(split_integer(8, 1)) == 8,\
+        "Sum of the parts should be equal to the original value."
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(8, 4) == [2, 2, 2, 2]
-    assert split_integer(10, 5) == [2, 2, 2, 2, 2]
+    assert split_integer(8, 4) == [2, 2, 2, 2],\
+        "Value should be split into equal parts when divisible."
+    assert split_integer(10, 5) == [2, 2, 2, 2, 2],\
+        "Value should be split into equal parts when divisible."
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(8, 1) == [8]
-    assert split_integer(15, 1) == [15]
+    assert split_integer(8, 1) == [8],\
+        ("When splitting into one part,"
+         " the result should equal the original value.")
+    assert split_integer(15, 1) == [15],\
+        ("When splitting into one part,"
+         " the result should equal the original value.")
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-    assert result == sorted(result)
-    assert result == [4, 4, 4, 5]
-
-    result = split_integer(32, 6)
-    assert result == sorted(result)
-    assert result == [5, 5, 5, 5, 6, 6]
+    assert (split_integer(17, 4)
+            == sorted(split_integer(17, 4)) == [4, 4, 4, 5]), \
+        "Parts should be sorted when they are not equal."
+    assert (split_integer(32, 6)
+            == sorted(split_integer(32, 6)) == [5, 5, 5, 5, 6, 6]), \
+        "Parts should be sorted when they are not equal."
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(3, 5) == [0, 0, 1, 1, 1]
+    assert split_integer(3, 5) == [0, 0, 1, 1, 1], \
+        ("When value is less than the number of parts,"
+         " the result should include zeros.")
