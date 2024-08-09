033039d984f86957a476309ffd982b69705ffb7c
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-09 17:47:52 +0300

    implement py-split-integer(fixed problems with flake8)

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -28,4 +28,5 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert (
         split_integer(3, 5) == [0, 0, 1, 1, 1]
-    ), "Should return a list with zeros and ones when value is less than number_of_parts"
+    ), ("Should return a list with zeros "
+        "and ones when value is less than number_of_parts")
