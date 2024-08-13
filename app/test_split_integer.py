a25789e7ac23d2f4aa02dad0aa0596bb53450cd9
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-08-13 15:40:52 +0300

    fix2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -15,8 +15,8 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert (
-        split_integer(17, 4) == [4, 4, 4, 5]
-    ), "Should split into 4 parts"
+        split_integer(10, 1) == [10]
+    ), "Should split into one part"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
