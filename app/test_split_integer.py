815fe1523af45c0f4e2ebf1b35338210c8928a54
Kulib <kulibchenkovalera@gmail.com>
2024-08-25 12:12:42 +0300

    third commit(fix second test)

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert len(split_integer(20, 20)) == 20
+    assert split_integer(20, 5) == [4, 4, 4, 4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
