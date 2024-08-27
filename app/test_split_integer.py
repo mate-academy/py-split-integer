7d5d8b3d646487a630cf72e58ce2990a0f87308c
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-08-27 22:33:51 +0300

    fixed flake8

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -11,7 +11,7 @@ from app.split_integer import split_integer
         (32, 6)
     ]
 )
-def test_sum_of_the_parts_should_be_equal_to_value(value, number_of_parts) -> None:
+def test_sum_of_the_parts_should_be_equal_to_value(value, number_of_parts) -> None: # noqa
     assert sum(split_integer(value, number_of_parts)) == value
 
 
@@ -23,7 +23,7 @@ def test_sum_of_the_parts_should_be_equal_to_value(value, number_of_parts) -> No
         (50, 2)
     ]
 )
-def test_should_split_into_equal_parts_when_value_divisible_by_parts(value, number_of_parts) -> None:
+def test_should_split_into_equal_parts_when_value_divisible_by_parts(value, number_of_parts) -> None: # noqa
     result = split_integer(value, number_of_parts)
     assert len(result) == 2 and result[0] == result[1]
 
@@ -37,7 +37,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(value, numb
         (20, 1),
     ]
 )
-def test_should_return_part_equals_to_value_when_split_into_one_part(value, number_of_parts) -> None:
+def test_should_return_part_equals_to_value_when_split_into_one_part(value, number_of_parts) -> None: # noqa
     result = split_integer(value, number_of_parts)
     assert len(result) == 1 and result[0] == value
 
@@ -50,7 +50,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part(value, numb
         (52, 8)
     ]
 )
-def test_parts_should_be_sorted_when_they_are_not_equal(value, number_of_parts) -> None:
+def test_parts_should_be_sorted_when_they_are_not_equal(value, number_of_parts) -> None: # noqa
     result = split_integer(value, number_of_parts)
     assert result == sorted(result)
 
@@ -63,6 +63,6 @@ def test_parts_should_be_sorted_when_they_are_not_equal(value, number_of_parts) 
         (6, 29)
     ]
 )
-def test_should_add_zeros_when_value_is_less_than_number_of_parts(value, number_of_parts) -> None:
+def test_should_add_zeros_when_value_is_less_than_number_of_parts(value, number_of_parts) -> None: # noqa
     result = split_integer(value, number_of_parts)
     assert set(result) == {0, 1}
