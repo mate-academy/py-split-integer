3818053a6fbb19fbff5982971dc6581981b6754b
Arthur <avers.target@gmail.com>
2024-09-02 17:34:58 +0200

    refactor: Simplify test_sum_of_the_parts_should_be_equal_to_value in test_split_integer.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,10 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 10
-    number_of_parts = 5
-    parts = split_integer(value, number_of_parts)
-    assert sum(parts) == value
+    assert sum(split_integer(10, 5)) == 10
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
