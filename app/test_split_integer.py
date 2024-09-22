fdc9a6eb6ec8c4a452cc16e61bff02df6f3ce6c7
Alex <54539kalashnikov@gmail.com>
2024-09-22 20:54:38 +0300

    Remove excess

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,18 +2,15 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    sum(split_integer(10, 5))
     assert sum(split_integer(10, 5)) == 10
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    split_integer(20, 5)
     assert split_integer(20, 5) == [4, 4, 4, 4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    result = split_integer(20, 1)
-    assert result[0] == 20
+    assert split_integer(20, 1) == [20]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
