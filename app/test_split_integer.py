039734366c38bd7c0e9f39785a189ef5ac5ec7c4
poznianski <poznianski@gmail.com>
2024-09-27 23:34:38 +0300

    sol

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,44 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    value = 17
+    number_of_parts = 4
+    result = split_integer(value, number_of_parts)
+    assert sum(result) == value
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    value = 12
+    number_of_parts = 3
+    result = split_integer(value, number_of_parts)
+    assert result == [4, 4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    value = 8
+    number_of_parts = 1
+    result = split_integer(value, number_of_parts)
+    assert result == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    value = 17
+    number_of_parts = 4
+    result = split_integer(value, number_of_parts)
+    assert result == sorted(result)
+    assert result == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    value = 2
+    number_of_parts = 5
+    result = split_integer(value, number_of_parts)
+    assert result == [0, 0, 0, 1, 1]
+
+
+def test_difference_between_max_and_min_should_be_less_or_equal_to_one() -> None:
+    value = 32
+    number_of_parts = 6
+    result = split_integer(value, number_of_parts)
+    assert max(result) - min(result) <= 1
+    assert result == [5, 5, 5, 5, 6, 6]
