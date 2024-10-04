1bbeb586f22fdd486719c211728aa8d0253ddb05
Andrii Uvarov <aaanyyy@gmail.com>
2024-10-04 12:36:20 +0200

    flake8 fixes

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -3,29 +3,30 @@ from app.split_integer import split_integer
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert (
-            split_integer(8, 4) == [2, 2, 2, 2]
+        split_integer(8, 4) == [2, 2, 2, 2]
     ), "result of splitting 8 to 4 parts should be equal to [2, 2, 2, 2]"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert (
-            split_integer(6, 2) == [3, 3]
-    ),  "result of splitting 6 to 2 part should be equal to [3, 3]"
+        split_integer(6, 2) == [3, 3]
+    ), "result of splitting 6 to 2 part should be equal to [3, 3]"
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert (
-            split_integer(8, 1) == [8]
+        split_integer(8, 1) == [8]
     ), "result of splitting 8 to one part should be equal to [8]"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert (
-            split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
-    ), "result of splitting 32 to 6 parts should be equal to [5, 5, 5, 5, 6, 6]"
+        split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    ), ("result of splitting 32 to 6 parts should be "
+        "equal to [5, 5, 5, 5, 6, 6]")
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert (
-            split_integer(1, 3) == [0, 0, 1]
+        split_integer(1, 3) == [0, 0, 1]
     ), "result of splitting 1 to 3 parts should be equal to [0, 0, 1]"
