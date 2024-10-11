5f1752bbc00173edad96fa374db2b8fe495ac69d
yura <pollo7964132@gmail.com>
2024-10-11 20:50:55 +0300

    implemented tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,29 @@
+import pytest
+
 from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    value = 17
+    number_of_parts = 4
+    assert sum(split_integer(17, 4)) == value
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    parts = split_integer(32, 6)
+    assert parts.count(6) == 2 and parts.count(5) == 4
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    parts = split_integer(8, 1)
+    assert parts == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    parts = split_integer(32, 6)
+    assert parts == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    parts = split_integer(1, 5)
+    assert parts == [0, 0, 0, 0, 1]
