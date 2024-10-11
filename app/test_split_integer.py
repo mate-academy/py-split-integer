ae5809ade4fc34b37674490837148aa59cf5017f
yura <pollo7964132@gmail.com>
2024-10-11 21:25:16 +0300

    fixed unnecessary varriables

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -8,7 +8,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    assert split_integer(30, 6) == [5, 5, 5, 5, 5, 5]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -20,5 +20,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    parts = split_integer(1, 5)
-    assert parts == [0, 0, 0, 0, 1]
+    assert split_integer(1, 5) == [0, 0, 0, 0, 1]
