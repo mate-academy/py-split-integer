8f84448987508dd5cd8f4329dd8da11a321bb147
roman <skyfoxwork@gmail.com>
2024-10-14 08:09:09 +0300

    Solution-py-split-integer v3.0

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,30 +2,20 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert (
-        sum(split_integer(6, 2)) == 6
-    ), "sum of the parts should be equal to value"
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert (
-            split_integer(30, 6) == [5, 5, 5, 5, 5, 5]
-    ), "should be split into equal parts when value divisible by parts"
+    assert split_integer(30, 6) == [5, 5, 5, 5, 5, 5]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert (
-        split_integer(5, 1) == [5]
-    ), "should be equal to value when split 1."
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert (
-        split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
-    ), "parts should be sorted when they are not equal"
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert (
-        split_integer(1, 5) == [0, 0, 0, 0, 1]
-    ), "should add zeros when value is less than number of part"
+    assert split_integer(1, 5) == [0, 0, 0, 0, 1]
