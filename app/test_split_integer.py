9cb5586cbd3e946adf80e15a5efc0a865368999a
roman <skyfoxwork@gmail.com>
2024-10-14 07:53:32 +0300

    Solution-py-split-integer v2.3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -9,7 +9,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert (
-        split_integer(6, 2) == [3, 3]
+        split_integer(6, 2) == 2
     ), "should be split into equal parts when value divisible by parts"
 
 
