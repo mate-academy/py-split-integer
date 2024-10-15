3873d8495e61caff186a3bbf19466ca65130cdb3
Aleksandr Mazur <a9p9m9@gmail.com>
2024-10-15 10:54:59 +0300

    Solution_3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,8 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    summa = sum(split_integer(17, 4))
-    assert summa == 17
+    assert split_integer(17, 4) == sum([4, 4, 4, 5])
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
