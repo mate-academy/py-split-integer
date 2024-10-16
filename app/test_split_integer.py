73343382e7ce348d31c0af58b23259070b657bec
olgas <olgashydlovska@gmail.com>
2024-10-16 09:12:29 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,41 +2,22 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 17
-    number_of_parts = 4
-    result = split_integer(value, number_of_parts)
-    assert sum(result) == value, f"Sum of parts {result} should equal {value}"
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 6
-    number_of_parts = 2
-    result = split_integer(value, number_of_parts)
-    assert result == [3, 3], f"Expected equal parts but got {result}"
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 8
-    number_of_parts = 1
-    result = split_integer(value, number_of_parts)
-    assert result == [8], (f"Expected single part with value"
-                           f" {value}, but got {result}")
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 17
-    number_of_parts = 4
-    result = split_integer(value, number_of_parts)
-    assert result == sorted(result), (f"Parts should be sorted, "
-                                      f"but got {result}")
-    assert max(result) - min(result) <= 1, \
-        "Difference between max and min parts should be <= 1"
+    assert split_integer(17, 4) == sorted(split_integer(17, 4)), "Parts should be sorted"
+    assert max(split_integer(17, 4)) - min(
+        split_integer(17, 4)) <= 1, "Difference between max and min parts should be <= 1"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 3
-    number_of_parts = 5
-    result = split_integer(value, number_of_parts)
-    assert result == [0, 0, 1, 1, 1], (f"Expected parts with zeros, "
-                                       f" but got {result}")
-    assert sum(result) == value, f"Sum of parts {result} should equal {value}"
+    assert split_integer(3, 5) == [0, 0, 1, 1, 1]
