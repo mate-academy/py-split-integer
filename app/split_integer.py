28672a31fc81b6686c92250c3722b18ece75b677
Sonya Savkova <silva.vvss12@gmail.com>
2024-10-16 14:21:44 +0300

    Solution

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,4 +1,7 @@
 def split_integer(value: int, number_of_parts: int) -> list:
+    if value < number_of_parts:
+        return [1] * value + [0] * (number_of_parts - value)
+
     parts = []
     for parts_left in range(number_of_parts, 0, -1):
         next_number = value // parts_left
