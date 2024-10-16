f6d080aab8182e69cc38109903edd2cf34a52482
Mike Shmygel <m.shmygel@gmail.com>
2024-10-16 21:30:23 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,9 +2,9 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert split_integer(32, 6) == 32, (
-        "Sum of parts should be equal to value"
-    )
+    assert sum(split_integer(32, 6)) == 32, (
+        "Sum of the function result with value {32} "
+        "and number_of_parts {6} should be equal to {32}")
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -15,17 +15,17 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 1) == [8], (
-        "Parts should equal to value"
+        "Result should be equal to value when split into one part"
     )
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6], (
-        "Parts should be sorted"
+        "Result list  should be sorted"
     )
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(3, 4) == [0, 0, 0, 0], (
-        "Value should be equal or greater than number_of_parts"
+    assert split_integer(3, 5) == [0, 0, 1, 1, 1], (
+        "Result should contain zeros when value is less than number of parts"
     )
