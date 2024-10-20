1816c43837ee91c42c194a736e0ac32adc678594
Matvii Safonov <matveysafonov2@gmail.com>
2024-10-20 23:20:36 +0300

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,20 @@
 from app.split_integer import split_integer
+import pytest
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
-
-
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
-
-
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
-
-
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
-
-
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+@pytest.mark.parametrize("value, number_of_parts, expected", [
+    (8, 1, [8]),
+    (6, 2, [3, 3]),
+    (17, 4, [4, 4, 4, 5]),
+    (32, 6, [5, 5, 5, 5, 6, 6]),
+    (10, 3, [3, 3, 4]),
+    (9, 4, [2, 2, 2, 3]),
+    (15, 5, [3, 3, 3, 3, 3]),
+    (20, 4, [5, 5, 5, 5]),
+    (7, 2, [3, 4]),
+    (14, 3, [4, 5, 5])
+])
+def test_split_integer(value: int,
+                       number_of_parts: int,
+                       expected: list) -> None:
+    assert split_integer(value, number_of_parts) == expected
