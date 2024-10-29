5bd092d7187abb549d698f7d3062e565b7148a4d
Andrii Ovod <ovodenok@mail.com>
2024-10-29 17:55:50 +0200

    Solution after check flake8

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -19,11 +19,11 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     result = split_integer(5, 3)
-    assert result == sorted(result), f"Expected result to be sorted but got {result}"
+    assert result == sorted(result), (f"Expected result to be sorted but got"
+                                      f" {result}")
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     result = split_integer(1, 4)
-    assert result == [0, 0, 0, 1], f"Expected result to be equal to 1, but got {result}"
-
-
+    assert result == [0, 0, 0, 1], (f"Expected result to be equal to 1, "
+                                    f"but got {result}")
