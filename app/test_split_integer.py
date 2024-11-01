0702d854a9baf8be6f90e638b8fa35433e940606
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-11-01 20:39:54 +0200

    fixed py-split-integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -14,7 +14,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(38, 10) == [6, 6, 6, 6, 7, 7]
+    assert split_integer(38, 5) == [6, 6, 6, 6, 7, 7]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
