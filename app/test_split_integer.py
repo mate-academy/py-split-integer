0a16d517aa6591528b1c324a0cdbd11e07bc9e3f
Nikita Ponomarov <wertyciti29@gmail.com>
2024-11-03 13:09:17 +0200

    solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,43 @@
+import pytest
+
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
-
-
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
-
-
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
-
-
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
-
-
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+@pytest.mark.parametrize(
+    "value,number_of_parts,result",
+    [
+        pytest.param(
+            8,
+            2,
+            [4, 4],
+            id="should split into equal parts when value divisible by parts"
+        ),
+        pytest.param(
+            8,
+            1,
+            [8],
+            id="should return part equals to value when split into one part"
+        ),
+        pytest.param(
+            9,
+            2,
+            [4, 5],
+            id="parts should be sorted when they are not equal"
+        ),
+        pytest.param(
+            3,
+            5,
+            [0, 0, 1, 1, 1],
+            id="should add zeros when value is less than number of parts"
+        ),
+    ]
+)
+def test_works_correctly(
+        value: int,
+        number_of_parts: int,
+        result: list
+) -> None:
+    assert (
+        split_integer(value, number_of_parts) == result
+    ), (f"The number {value} when divided into "
+        f"{number_of_parts} parts must be equal to {result}")
