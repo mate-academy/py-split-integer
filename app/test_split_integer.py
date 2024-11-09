4e280ac9b61e4b6773c0fa9be5764810ea51e177
fanni <fmalevych@zoolatech.com>
2024-11-09 18:50:11 +0200

    solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,20 @@
+import pytest
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
-
-
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
-
-
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
-
-
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
-
-
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+@pytest.mark.parametrize("value,parts_number,expected", [
+    pytest.param(8, 1, [8],
+                 id="Sum of the parts should be equal to value"),
+    pytest.param(8, 2, [4, 4],
+                 id="Should split into equal parts,"
+                    " when value divisible by parts"),
+    pytest.param(9, 1, [9],
+                 id="Should return part equals to value,"
+                    " when split into one part"),
+    pytest.param(17, 4, [4, 4, 4, 5],
+                 id="Parts should be sorted when they are not equal"),
+    pytest.param(2, 4, [0, 0, 1, 1],
+                 id="Should add zeros when value is less than number of parts")
+])
+def test_split_integer(value: int, parts_number: int, expected: list) -> None:
+    assert split_integer(value, parts_number) == expected
