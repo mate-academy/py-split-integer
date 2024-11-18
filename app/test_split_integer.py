ecf8e1a44f1d7266ade282b630ccd2812441b47f
Olha Shumska <olgashumska65@gmail.com>
2024-11-18 06:43:03 +0100

    corrections

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,27 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(52, 6)) == 52, "Sum of parts should be equal to value"
+    assert sum(split_integer(52, 6)) == 52, \
+        "Sum of parts should be equal to value"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert (split_integer(8, 2) == [4, 4]), f"Array should split value 8 on two equal parts not {split_integer(8, 2)}"
+    assert (split_integer(8, 2) == [4, 4]), \
+        (f"Array should split value 8 on two equal parts not "
+         f"{split_integer(8, 2)}")
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert (split_integer(9, 1) == [9]), f"When number of parts equal to 1, one part should be equal to value 9"
+    assert (split_integer(9, 1) == [9]), \
+        "When number of parts equal to 1, one part should be equal to value 9"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert (split_integer(32, 6) == [5, 5, 5, 5, 6, 6]), f"Parts should be sorted in ascending order"
+    assert (split_integer(32, 6) == [5, 5, 5, 5, 6, 6]), \
+        "Parts should be sorted in ascending order"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert (split_integer(3, 5) == [0, 0, 1, 1, 1]), "Array from value 3 and number of parts 5 should be equal [0, 0, 1, 1, 1]"
+    assert (split_integer(3, 5) == [0, 0, 1, 1, 1]), \
+        ("Array from value 3 and number of parts 5 should be equal "
+         "[0, 0, 1, 1, 1]")
