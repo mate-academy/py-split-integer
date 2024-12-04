6c4f67cfe0e7a6b62e2fefebd346644fefc530bb
Radeon <egorka22e40x@gmail.com>
2024-12-04 13:34:40 +0200

    Solution

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,7 +1,11 @@
 def split_integer(value: int, number_of_parts: int) -> list:
     parts = []
+    number = value
     for parts_left in range(number_of_parts, 0, -1):
-        next_number = value // parts_left
-        parts.append(value // parts_left)
-        value -= next_number
+        next_number = number // parts_left
+        parts.append(next_number)
+        number -= next_number
     return parts
+
+# print([32 // 6] * 6)
+# print(split_integer(28, 7))
--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,35 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    value = 32
+    number_of_parts = 6
+    split_num_lst = split_integer(value, number_of_parts)
+    assert sum(split_num_lst) == value
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    value = 28
+    number_of_parts = 7
+    split_num_lst = split_integer(value, number_of_parts)
+    assert split_num_lst == [4, 4, 4, 4, 4, 4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    value = 8
+    number_of_parts = 1
+    split_num_lst = split_integer(value, number_of_parts)
+    assert split_num_lst == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    value = 30
+    number_of_parts = 7
+    split_num_lst = split_integer(value, number_of_parts)
+    assert sorted(split_num_lst) == [4, 4, 4, 4, 4, 5, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    value = 6
+    number_of_parts = 8
+    split_num_lst = split_integer(value, number_of_parts)
+    assert sorted(split_num_lst) == [0, 0, 1, 1, 1, 1, 1, 1]
