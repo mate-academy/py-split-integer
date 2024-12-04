caf09a0c970610a583167a5039c34ce644bf40f6
Radeon <egorka22e40x@gmail.com>
2024-12-04 13:43:23 +0200

    change split_integer

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,11 +1,7 @@
 def split_integer(value: int, number_of_parts: int) -> list:
     parts = []
-    number = value
     for parts_left in range(number_of_parts, 0, -1):
-        next_number = number // parts_left
+        next_number = value // parts_left
         parts.append(next_number)
-        number -= next_number
+        value -= next_number
     return parts
-
-# print([32 // 6] * 6)
-# print(split_integer(28, 7))
