5395c9226dcd19b69a420844e4a60071e0aceed1
Vova <vova.gld05@gmail.com>
2024-12-18 18:10:50 +0200

    chanÐanget solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert len(split_integer(12, 4)) == 3
+    assert split_integer(12, 4).count(3) == 4
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
