3530dacdcc8de026c37fe67fc3c77b33965f84fc
shafran <shafran.andriy@gmail.com>
2024-12-19 14:07:25 +0200

    done all tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,15 +2,18 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert len(split_integer(8, 1)) == 1
+    result = split_integer(8, 3)
+    assert sum(result) == 8
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(6, 2) == [3, 3]
+    result = split_integer(6, 2)
+    assert result == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert len(split_integer(8, 1)) == 1
+    result = split_integer(8, 1)
+    assert result == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
