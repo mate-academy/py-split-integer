1234b8e88b102adae09a98994bd9f12d613d4788
Yurii Prydii <jurassicnec@gmail.com>
2024-12-21 22:54:41 +0200

    Solution

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,7 +1,13 @@
+
 def split_integer(value: int, number_of_parts: int) -> list:
     parts = []
     for parts_left in range(number_of_parts, 0, -1):
         next_number = value // parts_left
         parts.append(value // parts_left)
+        print(parts_left, parts, value)
         value -= next_number
+        # print(value)
     return parts
+
+
+split_integer(32, 33)
