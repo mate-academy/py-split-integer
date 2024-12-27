9b958cbc75c94ba6756a05ce90b3a41addb34ace
Taras <t.kudinov90@gmail.com>
2024-12-27 13:00:20 +0200

    Solved task with passed tests and with unnecessary variables avoiding.

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,20 +6,20 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    ext = split_integer(30, 5)
-    assert len(ext) == 5
+
+    assert len(split_integer(30, 5)) == 5
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    ext = split_integer(12, 1)
-    assert ext == [12]
+
+    assert split_integer(12, 1) == [12]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    ext = split_integer(34, 7)
-    assert ext == sorted(ext)
+
+    assert split_integer(34, 7) == sorted(split_integer(34, 7))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    ext = split_integer(7, 9)
-    assert ext == [0, 0, 1, 1, 1, 1, 1, 1, 1]
+
+    assert split_integer(7, 9) == [0, 0, 1, 1, 1, 1, 1, 1, 1]
