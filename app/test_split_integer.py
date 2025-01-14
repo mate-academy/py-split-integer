3c76efb09218dcbcf5abb91eef4be071717a6691
Olha Yukhnenko <pyolh36@gmail.com>
2025-01-14 13:54:20 +0200

    solved_task

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,33 @@
+import pytest
 from app.split_integer import split_integer
 
 
+def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
+    assert split_integer(8, 1) == [8]
+
+
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
-
-
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    assert split_integer(20, 5) == [4, 4, 4, 4, 4]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    assert split_integer(6, 8) == [0, 0, 1, 1, 1, 1, 1, 1]
+
+
+@pytest.mark.parametrize("value, number_of_parts, expected", [
+        (8, 1, [8]),
+        (6, 2,  [3, 3]),
+        (20, 5, [4, 4, 4, 4, 4]),
+        (32, 6, [5, 5, 5, 5, 6, 6]),
+        (6, 8, [0, 0, 1, 1, 1, 1, 1, 1]),
+])
+def test_various_case(value, number_of_parts, expected):
+    assert split_integer(value, number_of_parts) == expected
