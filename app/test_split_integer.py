012c0cc073ed77af972ed202071e99098217018d
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-20 10:50:44 +0200

    compltet tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,29 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    input_val = 19
+    parts = 3
+    assert split_integer(input_val, parts) == [6,6,7]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    input_val = 25
+    parts = 5
+    assert split_integer(input_val, parts) == [5,5,5,5,5]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    assert split_integer(10, 1) == [10]
+
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    input_val = 26
+    parts = 6
+    assert split_integer(input_val, parts) == [4,4,4,4,5,5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    input_val = 2
+    parts = 5
+    assert split_integer(input_val, parts) == [0,0,0,1,1]
