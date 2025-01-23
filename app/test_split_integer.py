d6aab5b2f3685b3dc14c92e6058231ee5f9dfd75
Olena Bukhova <lena.bukhova2005@gmail.com>
2025-01-23 22:01:36 +0100

    Fixed flake8

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -9,7 +9,6 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
-
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 1) == [8]
 
