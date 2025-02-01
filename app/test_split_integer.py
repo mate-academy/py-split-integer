d54d95e7215d09703c4e8901dc28116ebbf17d2a
oh <wish.master.mvp@gmail.com>
2025-02-01 23:24:14 +0200

    fix format

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -5,7 +5,9 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     value = 8
     number_of_parts = 3
     result = split_integer(value, number_of_parts)
-    assert sum(result) == value, "The sum of parts is not equal to the original value"
+    assert sum(result) == value, (
+        "The sum of parts is not equal to the original value"
+    )
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -27,17 +29,21 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     number_of_parts = 4
     result = split_integer(value, number_of_parts)
     assert result == sorted(result), f"Result list is not sorted: {result}"
-    assert max(result) - min(result) <= 1, "Difference between max and min parts is greater than 1"
-
+    assert max(result) - min(result) <= 1, (
+        "Difference between max and min parts is greater than 1"
+    )
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     value = 2
     number_of_parts = 5
     result = split_integer(value, number_of_parts)
-    assert sum(result) == value, "The sum of parts is not equal to the original value"
+    assert sum(result) == value, (
+        "The sum of parts is not equal to the original value"
+    )
     assert len(result) == number_of_parts, (
-        f"Result list length ({len(result)}) does not match the number of parts ({number_of_parts})"
+        f"Result list length ({len(result)}) does not match "
+        f"the number of parts ({number_of_parts})"
     )
     assert result == sorted(result), f"Result list is not sorted: {result}"
     assert max(result) - min(result) <= 1, (
