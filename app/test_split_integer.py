bf28ee3ae2a34c5501523c62b897e43594541757
Vinohradov Volodymyr <eXpertt13@gmail.com>
2025-02-04 23:21:13 +0200

    complete tests for split integer.

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,26 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    assert sum(split_integer(20, 4)) == 20
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    assert len(set(split_integer(10, 2))) == 1
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    assert split_integer(32, 1) == [32]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    result = split_integer(32, 6)
+    assert result == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    assert split_integer(3, 8).count(0) == 8 - 3
+
+
+def test_difference_between_min_max_number_in_parts_no_more_one() -> None:
+    result = split_integer(27, 4)
+    assert max(result) - min(result) <= 1
