f933d07f27fafbe1122291a78027fef41e150c14
Marek Smolana <msmolana@gmail.com>
2025-02-08 17:31:11 +0100

    fixed flake errors

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,15 +1,18 @@
 import pytest
 from app.split_integer import split_integer
 
+
 @pytest.mark.parametrize(
     "value,number_of_parts,expected_result",
     [
         [10, 3, [3, 3, 4]],
         [28, 9, [3, 3, 3, 3, 3, 3, 3, 3, 4]]
-        
     ]
 )
-def test_sum_of_the_parts_should_be_equal_to_value(value: int, number_of_parts: int, expected_result: list) -> None:
+def test_sum_of_the_parts_should_be_equal_to_value(
+        value: int,
+        number_of_parts: int,
+        expected_result: list) -> None:
     act_result = split_integer(value, number_of_parts)
     assert act_result == expected_result
 
@@ -21,9 +24,13 @@ def test_sum_of_the_parts_should_be_equal_to_value(value: int, number_of_parts: 
         [28, 9, [3, 3, 3, 3, 3, 3, 3, 3, 4]]
     ]
 )
-def test_should_split_into_equal_parts_when_value_divisible_by_parts(value: int, number_of_parts: int, expected_result: list) -> None:
+def test_should_split_into_equal_parts_when_value_divisible_by_parts(
+        value: int,
+        number_of_parts: int,
+        expected_result: list) -> None:
     act_result = split_integer(value, number_of_parts)
     assert act_result == expected_result
+
 
 @pytest.mark.parametrize(
     "value,number_of_parts,expected_result",
@@ -32,7 +39,10 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(value: int,
         [15, 1, [15]]
     ]
 )
-def test_should_return_part_equals_to_value_when_split_into_one_part(value: int, number_of_parts: int, expected_result: list) -> None:
+def test_should_return_part_equals_to_value_when_split_into_one_part(
+        value: int,
+        number_of_parts: int,
+        expected_result: list) -> None:
     act_result = split_integer(value, number_of_parts)
     assert act_result == expected_result
 
