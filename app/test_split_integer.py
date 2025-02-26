d9ff0b50fd1c8756420b5ae71cff6f715d7acbdf
Nikita-pyth <nicksan2002@gmail.com>
2025-02-26 16:19:50 +0200

    flake8 adressed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -4,18 +4,22 @@ from app.split_integer import split_integer
 
 
 @pytest.mark.parametrize(
-    "a,b,result",
+    "first,second,result",
     [
         (8, 2, 8),
         (1, 1, 1)
     ]
 )
-def test_sum_of_the_parts_should_be_equal_to_value(a: int, b: int, result: int) -> None:
-    assert sum(split_integer(a, b)) == result
+def test_sum_of_the_parts_should_be_equal_to_value(
+        first: int,
+        second: int,
+        result: int
+) -> None:
+    assert sum(split_integer(first, second)) == result
 
 
 @pytest.mark.parametrize(
-    "a,b,result",
+    "first,second,result",
     [
         (8, 2, [4, 4]),
         (1, 1, [1]),
@@ -23,15 +27,15 @@ def test_sum_of_the_parts_should_be_equal_to_value(a: int, b: int, result: int) 
     ]
 )
 def test_should_split_into_equal_parts_when_value_divisible_by_parts(
-        a: int,
-        b: int,
+        first: int,
+        second: int,
         result: list[int]
 ) -> None:
-    assert split_integer(a, b) == result
+    assert split_integer(first, second) == result
 
 
 @pytest.mark.parametrize(
-    "a,b,result",
+    "first,second,result",
     [
         (8, 1, [8]),
         (1, 1, [1]),
@@ -39,38 +43,38 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(
     ]
 )
 def test_should_return_part_equals_to_value_when_split_into_one_part(
-        a: int,
-        b: int,
+        first: int,
+        second: int,
         result: list[int]
 ) -> None:
-    assert split_integer(a, b) == result
+    assert split_integer(first, second) == result
 
 
 @pytest.mark.parametrize(
-    "a,b,result",
+    "first,second,result",
     [
         (7, 3, [2, 2, 3]),
         (20, 3, [6, 7, 7])
     ]
 )
 def test_parts_should_be_sorted_when_they_are_not_equal(
-        a: int,
-        b: int,
+        first: int,
+        second: int,
         result: list[int]
 ) -> None:
-    assert split_integer(a, b) == result
+    assert split_integer(first, second) == result
 
 
 @pytest.mark.parametrize(
-    "a,b,result",
+    "first,second,result",
     [
         (2, 5, [0, 0, 0, 1, 1]),
         (3, 4, [0, 1, 1, 1]),
     ]
 )
 def test_should_add_zeros_when_value_is_less_than_number_of_parts(
-        a: int,
-        b: int,
+        first: int,
+        second: int,
         result: list[int]
 ) -> None:
-    assert split_integer(a, b) == result
+    assert split_integer(first, second) == result
