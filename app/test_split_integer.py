535204660cdb2c3a22f3a182cbc24c23d5d34835
Volodymyr Kolomiiets <valdi6014@gmail.com>
2025-02-28 20:02:23 +0200

    Fixed flake8 errors

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,12 +1,14 @@
-from app.split_integer import split_integer
 import pytest
+from app.split_integer import split_integer
 
 
 @pytest.mark.parametrize(
     "value, number_of_parts, expected_sum",
     [
-        pytest.param(17, 4, 17, 
-        id="Test checks sum of the parts is equal to value")
+        pytest.param(
+            17, 4, 17,
+            id="checks sum of the parts is equal to value"
+        )
     ]
 )
 def test_sum_of_the_parts_should_be_equal_to_value(
@@ -21,8 +23,10 @@ def test_sum_of_the_parts_should_be_equal_to_value(
 @pytest.mark.parametrize(
     "value, number_of_parts, expected_result",
     [
-        pytest.param(6, 2, [3, 3], 
-        id="Test should split into equal parts when value divisible by parts")
+        pytest.param(
+            6, 2, [3, 3],
+            id="Split into equal parts when value divisible by parts"
+        )
     ]
 )
 def test_should_split_into_equal_parts_when_value_divisible_by_parts(
@@ -36,8 +40,10 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(
 @pytest.mark.parametrize(
     "value, number_of_parts, expected_result",
     [
-        pytest.param(8, 1, [8], 
-        id="Test should return value as single element when split into one part")
+        pytest.param(
+            8, 1, [8],
+            id="Should return value as single element when split into one part"
+        )
     ]
 )
 def test_should_return_part_equals_to_value_when_split_into_one_part(
@@ -51,8 +57,10 @@ def test_should_return_part_equals_to_value_when_split_into_one_part(
 @pytest.mark.parametrize(
     "value, number_of_parts, expected_result",
     [
-        pytest.param(17, 4, [4, 4, 4, 5], 
-        id="Test parts should be sorted when they are not equal")
+        pytest.param(
+            17, 4, [4, 4, 4, 5],
+            id="Parts should be sorted when they are not equal"
+        )
     ]
 )
 def test_parts_should_be_sorted_when_they_are_not_equal(
@@ -69,8 +77,10 @@ def test_parts_should_be_sorted_when_they_are_not_equal(
 @pytest.mark.parametrize(
     "value, number_of_parts, expected_result",
     [
-        pytest.param(2, 4, [0, 0, 1, 1], 
-        id="Test should add zeros when value is less than number of parts")
+        pytest.param(
+            2, 4, [0, 0, 1, 1],
+            id="Should add zeros when value is less than number of parts"
+        )
     ]
 )
 def test_should_add_zeros_when_value_is_less_than_number_of_parts(
