135b36a2ed5f1b8368174bb637adaac78abe71f9
Maria Hrabovska <mariya76om@gmail.com>
2025-03-02 16:14:43 +0200

    Solution

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,7 +1,9 @@
 def split_integer(value: int, number_of_parts: int) -> list:
-    parts = []
-    for parts_left in range(number_of_parts, 0, -1):
-        next_number = value // parts_left
-        parts.append(value // parts_left)
-        value -= next_number
-    return parts
+    base_value = value // number_of_parts
+    remainder = value % number_of_parts
+    parts = [base_value] * number_of_parts
+
+    for i in range(remainder):
+        parts[i] += 1  # Розподіляємо залишок на перші елементи
+
+    return sorted(parts)  # Гарантуємо, що список відсортований
