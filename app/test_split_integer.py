415681ff9074dbfbe0c9f9c962c465ca150f6f3f
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-03-11 12:57:15 +0200

    Absolutely final commit

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -12,13 +12,13 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(8, 1) == 8,\
+    assert split_integer(8, 1) == [8],\
         "Part should be equal to value when split into one part"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     result = split_integer(32, 6)
-    assert result == sorted(result),\
+    assert result == [5, 5, 5, 5, 6, 6],\
         "Parts should be sorted when they aren't equal"
 
 
