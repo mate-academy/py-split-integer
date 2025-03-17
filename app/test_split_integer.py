0ad875aee0d72d15f3d0b0690a2edc35ebc735df
Iryna <irakirvas78@gmail.com>
2025-03-17 16:54:23 +0300

    Implement py-split-integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,25 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    ls = split_integer(6, 3)
+    assert sum(ls) == 6
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    ls = split_integer(6, 3)
+    assert len(ls) == 3
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    ls = split_integer(6, 1)
+    assert len(ls) == 1 and ls[0] == 6
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    ls = split_integer(32, 6)
+    assert ls == sorted(ls)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    ls = split_integer(4, 5)
+    assert ls == [0, 1, 1, 1, 1]
