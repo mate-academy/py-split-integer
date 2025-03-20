98c2b314b535fb37e6aec7508999456408ff44b5
artem <krenky.spenky@gmail.com>
2025-03-20 17:08:52 +0200

    'commit'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,30 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    value, number_of_parts = 15, 4
+    result = split_integer(value, number_of_parts)
+    assert sum(result) == value
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    value, number_of_parts = 12, 3
+    result = split_integer(value, number_of_parts)
+    assert result == [4, 4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    value, number_of_parts = 7, 1
+    result = split_integer(value, number_of_parts)
+    assert result == [7]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    value, number_of_parts = 10, 3
+    result = split_integer(value, number_of_parts)
+    assert result == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    value, number_of_parts = 3, 5
+    result = split_integer(value, number_of_parts)
+    assert result == [0, 0, 1, 1, 1]
