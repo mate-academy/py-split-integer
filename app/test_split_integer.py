8a6378d589629f95c4811ca89ce4ad309dd92546
slradbez <slradbez@gmail.com>
2025-03-26 19:12:19 +0100

    solution 2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,41 +2,28 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 10
-    parts = 3
-    result = split_integer(value, parts)
-    assert sum(result) == value, "Сумма частей должна быть равна value"
+    assert sum(split_integer(10, 3)) == 10,\
+        "The sum of the parts should equal the value"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    value = 12
-    parts = 4
-    result = split_integer(value, parts)
-    assert result == [3, 3, 3, 3],\
-        "Не делит значение на равные части, если оно делится без остатка"
+    assert split_integer(12, 4) == [3, 3, 3, 3],\
+        "Does not split value into equal parts when divisible by number of parts"
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    value = 8
-    parts = 1
-    result = split_integer(value, parts)
-    assert result == [8],\
-        "Должен возвращать единственную часть, равную value, если parts=1"
+    assert split_integer(8, 1) == [8],\
+        "Should return a single part equal to the value when number of parts is 1"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 17
-    parts = 4
-    result = split_integer(value, parts)
+    result = split_integer(17, 4)
     assert result == sorted(result),\
-        "Части должны быть отсортированы по возрастанию"
+        "Parts should be sorted in ascending order"
     assert max(result) - min(result) <= 1,\
-        "Разница между максимальным и минимальным элементами должна быть ≤ 1"
+        "Difference between max and min elements should be ≤ 1"
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    value = 3
-    parts = 5
-    result = split_integer(value, parts)
-    assert result == [0, 0, 1, 1, 1],\
-        "Должен добавлять нули, если value < parts"
+    assert split_integer(3, 5) == [0, 0, 1, 1, 1],\
+        "Should add zeros when value is less than number of parts"
