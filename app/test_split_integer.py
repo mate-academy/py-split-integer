5f0e0d7b561c61d7e6f7774441603db51407910b
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-03 18:10:37 +0300

    test_split_integer.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,18 @@
 from app.split_integer import split_integer
 
-
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(10, 5)) == 10
-
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert split_integer(10, 2) == [5, 5]
 
-
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(10, 1) == [10]
 
-
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(10, 6) == [1, 1, 2, 2, 2, 2]
-
+    result = split_integer(10, 6)
+    expected = [1, 1, 2, 2, 2, 2]
+    assert sorted(result) == expected, "The parts should be sorted correctly"
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(10, 15) == [0] * 5 + [1] * 10
