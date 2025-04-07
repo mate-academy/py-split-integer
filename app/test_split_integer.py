115b640ef04aebb1c36e31bd4f700ebac4692ce3
Denys Halytskyi <denys.halytskyi17@gmail.com>
2025-04-07 21:47:51 +0300

    'Solution2'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -27,6 +27,7 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert res == sorted(res)
 
     res = split_integer(3, 2)
+    assert res == sorted(res)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
