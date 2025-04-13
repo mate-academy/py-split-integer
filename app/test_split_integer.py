191f8350399c7641d37022814f70517484c3e1c6
Viacheslav Tykhenkyi <59470677+4zik88@users.noreply.github.com>
2025-04-13 18:41:49 +0300

    Fix after review 3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,8 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result = split_integer(17, 4)
-    assert sum(result) == 17
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -15,14 +14,12 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-    assert result == [4, 4, 4, 5]
-    assert result == sorted(result)
+    assert split_integer(17, 4) == [4, 4, 4, 5]
+    assert split_integer(17, 4) == sorted(split_integer(17, 4))
 
 
 def test_max_min_difference_should_be_at_most_one() -> None:
-    result = split_integer(32, 6)
-    assert max(result) - min(result) <= 1
+    assert max(split_integer(32, 6)) - min(split_integer(32, 6)) <= 1
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
