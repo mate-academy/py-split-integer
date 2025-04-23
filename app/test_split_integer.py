a0302b2c6e8c82e27a507859edbd1bc19ff05dfb
Vladyslav Pihol <freepgvlad@gmail.com>
2025-04-23 15:12:37 +0300

    Solution1

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -20,7 +20,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     value = 6
-    number_of_parts = 2
+    number_of_parts = 1
     parts = split_integer(value, number_of_parts)
     assert sum(parts) == value, \
         "Test should return part equals to value when split into one part"
