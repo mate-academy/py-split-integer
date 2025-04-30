f0938f283b0adb0babbcc47ae9278106a023269f
Andriy125 <yaruta.andriy@gmail.com>
2025-04-30 20:47:58 +0300

    chore: fix code style issues reported by flake8

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,11 +6,11 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(6,2) == [3, 3]
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-     assert split_integer(8,1) == [8]
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
