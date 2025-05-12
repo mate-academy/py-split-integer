ea31c6a7bf64a351e757ae5ad036c7dbb31276f8
Ermolenko Andrey <f1nd6r@gmail.com>
2025-05-12 16:06:11 +0300

    Solution

--- app/split_integer.py
+++ app/split_integer.py
@@ -1,5 +1,7 @@
 def split_integer(value: int, number_of_parts: int) -> list:
     parts = []
+    if number_of_parts <= 0:
+        raise ValueError("Number of parts must be greater than zero.")
     for parts_left in range(number_of_parts, 0, -1):
         next_number = value // parts_left
         parts.append(value // parts_left)
