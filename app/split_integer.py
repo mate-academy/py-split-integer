c16c648b9b94833100585eaedd3651c7b26d831c
Ermolenko Andrey <f1nd6r@gmail.com>
2025-05-12 18:22:14 +0300

    Solution3

--- app/split_integer.py
+++ app/split_integer.py
@@ -4,7 +4,5 @@ def split_integer(value: int, number_of_parts: int) -> list:
         raise ValueError("Number of parts must be greater than zero.")
     base_part = value // number_of_parts
     remainder = value % number_of_parts
-    parts = [base_part] * number_of_parts
-    for i in range(remainder):
-        parts[i] += 1
+    parts = [base_part + 1] * remainder + [base_part] * (number_of_parts - remainder)
     return parts
