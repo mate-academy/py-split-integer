42a14f64e5050580fc80820905c0716ff3baac32
= <=>
2025-05-20 23:39:03 +0300

    implement py-split-integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,26 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    result = split_integer(17, 4)
+    assert sum(result) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    result = split_integer(20, 4)
+    assert result == [5, 5, 5, 5]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    result = split_integer(8, 1)
+    assert result == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    result = split_integer(32, 6)
+    assert result == sorted(result)
+    assert max(result) - min(result) <= 1
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    result = split_integer(3, 5)
+    assert result == [0, 0, 1, 1, 1]  # Sorted, sum = 3, parts = 5, difference <= 1
