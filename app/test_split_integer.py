511148d8365a19b927550ccc5042a6c3eb74d323
Vitalii Mushta <vitaliimushta1993@gmail.com>
2025-05-21 15:26:48 +0200

    Solution2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -15,7 +15,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     value = 8
-    number_of_parts = 4
+    number_of_parts = 1
     assert len(split_integer(value, number_of_parts)) == number_of_parts
 
 
