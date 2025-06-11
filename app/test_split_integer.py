24b559bf7f7b8f22b381052de55b264644df1a9d
kravetst <taraskravetc28@gmail.com>
2025-06-11 16:28:37 +0300

    finally all the tests passed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -11,7 +11,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(25, 1) == [25]
+    assert split_integer(17, 1) == [17]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -19,4 +19,14 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(4, 8) == [0, 0, 0, 0, 1, 1, 1, 1]
+    assert split_integer(4, 6) == [0, 0, 1, 1, 1, 1]
+
+
+def test_parts_difference_should_be_less_or_equal_one() -> None:
+    result = split_integer(123, 17)
+    assert max(result) - min(result) <= 1
+
+
+def test_remainder_should_be_added_to_first_elements() -> None:
+    result = split_integer(8, 3)
+    assert result == [2, 3, 3]
